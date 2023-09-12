c89d3e0a42603f148417d64bb27bc7a41846ea80
jt <jt.turner@knexusresearch.com>
2023-09-12 14:55:42 -0400

    adding in state fields from converters

--- components/decision_analyzer/monte_carlo/monte_carlo_analyzer.py
+++ components/decision_analyzer/monte_carlo/monte_carlo_analyzer.py
@@ -1,6 +1,7 @@
 from domain.internal import Probe, Scenario, DecisionMetrics, DecisionMetric
 from components import DecisionAnalyzer
 import components.decision_analyzer.monte_carlo.mc_sim as mcsim
+import components.decision_analyzer.monte_carlo.tinymed.ta3_converter as ta3_conv
 import pickle as pkl
 import util.logger
 
@@ -12,14 +13,14 @@ class MonteCarloAnalyzer(DecisionAnalyzer):
         super().__init__()
         self.trained_tree = trained_tree
         if self.trained_tree is None:
-            self.trained_tree = pkl.load(open('data/ta3int/tree.pkl', 'rb'))
+            self.trained_tree = pkl.load(open('../data/ta3int/tree.pkl', 'rb'))
             logger.debug("TRAINED MC DECISION TREE LOADED.")
 
     def analyze(self, scen: Scenario, probe: Probe) -> dict[str, DecisionMetrics]:
         analysis = {}
         for decision in probe.decisions:
             metrics: DecisionMetrics = {"Severity": DecisionMetric(name="Severity", description="Severity of all injuries across all casualties",
-                                                                 type=type(float), value=3)}
+                                                                 type=type(float), value=3.0)}
             decision.metrics.update(metrics)
             analysis[decision.id_] = metrics
         return analysis
--- scripts/tinymed_runner.py
+++ scripts/tinymed_runner.py
@@ -5,10 +5,12 @@ import components.decision_analyzer.monte_carlo.tinymed.medactions as medact
 import time
 import numpy as np
 import datetime
+
+from runner import TA3Driver, TA3Client
 from util import logger
 import pickle as pkl
 
-save = True
+save = False
 
 if __name__ == '__main__':
     selection_function = mcsim.mc_tree.select_node_eetrade
@@ -21,21 +23,35 @@ if __name__ == '__main__':
     root = mcsim.MCStateNode(init_tiny_state)
     tree = mcsim.MonteCarloTree(sim, [root], node_selector=selection_function)
 
+    client = TA3Client()
+    sid = client.start_session(f'TAD')
+    logger.info(f"Started Session-{sid}")
+
     sim_times = []
-    rollouts = 10000
+    rollouts = 1000
     depth = 9
-    for i in range(rollouts):
-        sim_start = time.time()
-        result = tree.rollout(max_depth=depth)
-        end_time = time.time()
-        simulation_time = end_time - sim_start
-        sim_times.append(simulation_time)
-        if i % 5000 == 0 and i != 0:
-            logger.debug("%d rollouts complete" % i)
-    total_time = sum(sim_times)
-    avg = np.mean(sim_times)
-    logger.debug("%d rollouts of depth %d took %s time (%s avg)" % (rollouts, depth,
-                                                                    datetime.timedelta(seconds=total_time),
-                                                                    datetime.timedelta(seconds=avg)))
+
+    scen = client.start_scenario()
+    if scen is None:
+        logger.info("Session Complete!")
+    logger.info(f"Started Scenario-{scen.id}")
+    while True:
+        probe = client.get_probe()
+        print('wakka')
+
+
+    # for i in range(rollouts):
+    #     sim_start = time.time()
+    #     result = tree.rollout(max_depth=depth)
+    #     end_time = time.time()
+    #     simulation_time = end_time - sim_start
+    #     sim_times.append(simulation_time)
+    #     if i % 500 == 0 and i != 0:
+    #         logger.debug("%d rollouts complete" % i)
+    # total_time = sum(sim_times)
+    # avg = np.mean(sim_times)
+    # logger.debug("%d rollouts of depth %d took %s time (%s avg)" % (rollouts, depth,
+    #                                                                 datetime.timedelta(seconds=total_time),
+    #                                                                 datetime.timedelta(seconds=avg)))
     if save:
         pkl.dump(tree, open('tree.pkl', 'wb'))
