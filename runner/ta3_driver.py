b15002f485636db5eb2cb5fb584749dd47c311b8
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2023-09-13 09:40:08 -0400

    Added EventBasedDiagnosisAnalyzer to components to run.

--- runner/ta3_driver.py
+++ runner/ta3_driver.py
@@ -3,6 +3,7 @@ from components.decision_selector.default import HumanDecisionSelector
 from components.decision_selector.default import BaselineDecisionSelector
 from components.elaborator.default import TA3Elaborator
 from components.decision_analyzer.default import BaselineDecisionAnalyzer
+from components.decision_analyzer.event_based_diagnosis.ebd_analyzer import EventBasedDiagnosisAnalyzer
 from .driver import Driver
 
 
@@ -13,8 +14,8 @@ class TA3Driver(Driver):
         else:
             selector = BaselineDecisionSelector()
         elaborator = TA3Elaborator()
-        analyzer = BaselineDecisionAnalyzer()
-        super().__init__(elaborator, selector, [analyzer])
+        analyzer1 = EventBasedDiagnosisAnalyzer()
+        super().__init__(elaborator, selector, [analyzer1])
 
     def _extract_state(self, dict_state: dict):
         return TA3State.from_dict(dict_state)
