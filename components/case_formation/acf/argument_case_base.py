35aac34ed31693610d9fe17aaef62d0b7f8e3730
cr625 <cr625@drexel.edu>
2023-09-14 03:27:49 -0400

    updated to create case base from extended cases

--- components/case_formation/acf/argument_case_base.py
+++ components/case_formation/acf/argument_case_base.py
@@ -6,26 +6,73 @@ from skrebate import ReliefF
 import warnings
 import json
 import os
-
+import sys
+import yaml
+sys.path.append('.')
+from domain.internal import Probe, State, Decision, KDMAs, KDMA, Scenario, DecisionMetric
+from components import DecisionAnalyzer
+from components.decision_analyzer import *
+# there will probably be a default here. Also, we will probably just call Decision Analyzer
+# python imports are weird
+from components.decision_analyzer.event_based_diagnosis.ebd_analyzer import EventBasedDiagnosisAnalyzer
+from components.decision_selector.cbr.cbr_decision_selector import CBRDecisionSelector, Case
+from components.hra import hra
+from components.hra import *
 
 warnings.filterwarnings('ignore')
-
-CASUALTY_LIST = [f"Casualty {c}" for c in ['A', 'B', 'C', 'D', 'E', 'F']]
-TREATMENT_LIST = ['Treatment 1', 'Treatment 2', 'Treatment 3', 'Treatment 4', 'Treatment 5',
-                  'Treatment 6']
-
-
 def get_data_dir():
     return os.path.join(os.path.dirname(__file__), 'data')
 
 def get_output_dir():
     return os.path.join(os.path.dirname(__file__), 'output')
- 
+
+CASUALTY_LIST = [f"Casualty {c}" for c in ['A', 'B', 'C', 'D', 'E', 'F']]
+TREATMENT_LIST = ['Treatment 1', 'Treatment 2', 'Treatment 3', 'Treatment 4', 'Treatment 5', 'Treatment 6']
+
+'''
+in the scenario, there are two sets of casualties, one for each vehicle
+if we are going to use the supplied data, this is a separate set of probes
+
+TREATMENT_LIST = ['NeedleDecomp', 'HemorrhageControl', 'IvFluids', 'IntraossDevice', 'BloodProducts','TranexamicAcid', 'ChestSeal', 'SalineLock', 'Medications', 'Airway']
+# We could read these from the yaml file or the data file
+'''
+labels = yaml.load(open(os.path.join(get_data_dir() + '/scenario.yaml'), 'r'), Loader=yaml.Loader)
+
+class ArgumentCaseProbe:
+    
+    def __init__(self, data):
+        self.data = data        
+        #print(self.data.columns)
+        self.probes = []
+
+    # we can replace this with ingesting the data as internal    
+    # create the mvp2 probes in the format of the internal Probe
+    def create_mvp2_probes(self):
+        probes = []
+        for index, row in self.data.iterrows():
+            # Probe parameters: id_, state, prompt            
+            probe = Probe(id_=row['session_id'], state=row['probe_id'], prompt=row['probe_id'])
+            
+            kdmas_row = str(row['mission']) + " " +  str(row['denial']) + " " +  str(row['risktol']) + str(row['timeurg'])
+            probe.decisions = [Decision(id_=1, value=row['choice'], kdmas=kdmas_row,)]
+            probes.append(probe)
+        return probes
+
+    def create_argument_case_probes(self):        
+        probes = []
+        # Background, Treatment, Scenario, Decision, Mission, Risk aversion
+        for index, row in self.data.iterrows():
+            # Probe parameters: id_, state, prompt            
+            probe = Probe(id_=row['CaseNumber'], state=row['Background'], prompt=row['Scenario'])
+            kdmas_row = str(row['Mission']) + " " +  str(row['Risk aversion'])
+            probe.decisions = [Decision(id_=row['CaseNumber'], value=row['Decision'], kdmas=kdmas_row,)]
+            probes.append(probe)
+        return probes
+
 def partial_cases():
     df_partial_cases = pd.read_csv(get_data_dir() + r"/ACF-input.csv")
     df_partial_cases.insert(loc=0, column = 'CaseNumber', value = np.arange(df_partial_cases.shape[0])) 
     df_partial_cases.to_csv(get_output_dir() + r"/partial_cases.csv", index=False)
-    #print(df_partial_cases)
     return df_partial_cases
 
 def elab_cases(df_partial_cases):
@@ -63,9 +110,9 @@ def case_expansion(df_elab_cases):
     applicable_decisions = json.load(open(get_data_dir() + r"/applicable_decisions.json"))
     scenario = json.load(open(get_data_dir() + r"/scenario.json"))
     survival_rate = json.load(open(get_data_dir() + r"/survival_rate.json"))
-    df_train_cases = []
+    df_extended_cases = []
 
-    #these are the names of the columns to be added in df_train_cases csv file
+    #these are the names of the columns to be added in df_extended_cases csv file
     decision_name = ["Treatment 1", "Treatment 2", "Treatment 3", "Treatment 4", "Treatment 5", "Treatment 6"]
     treatment_rules = ["if Casualty A then treatment 1, treatment 2, treatment 3","if Casualty B then treatment 1, treatment 2, treatment 3", "if Casualty C then treatment 1, treatment 2, treatment 3", "if Casualty D then treatment 4, treatment 5, treatment 6", "if Casualty E then treatment 4, treatment 5, treatment 6","if Casualty F then treatment 2, treatment 3, treatment 4, treatment 5, treatment 6"]
     survival_rate_name = ["if safe area then treatment 1 survival rate is high"]
@@ -86,19 +133,19 @@ def case_expansion(df_elab_cases):
         else:
             new_features.extend([0, 1 if scenario["danger"][patient].get("Treatment 6") == None else 1])
         new_features.extend([1,0] if row["Risk aversion"]>=6 else [0,1])
-        df_train_cases.append(new_features)
+        df_extended_cases.append(new_features)
         
-    df_train_cases = pd.DataFrame(df_train_cases, columns= decision_name + treatment_rules  + survival_rate_name + scenario_name)
+    df_extended_cases = pd.DataFrame(df_extended_cases, columns= decision_name + treatment_rules  + survival_rate_name + scenario_name)
     df_elab_cases["Background"] = pd.factorize(df_elab_cases["Background"])[0]
     df_elab_cases["PatientTreated"] = df_elab_cases["PatientTreated"].map(CASUALTY_LIST.index)
     df_elab_cases["Scenario"] = pd.factorize(df_elab_cases["Scenario"])[0]
     df_elab_cases["Decision"] = df_elab_cases["Decision"].map(TREATMENT_LIST.index)
-    df_train_cases = pd.concat([df_elab_cases,df_train_cases], axis=1)
+    df_extended_cases = pd.concat([df_elab_cases,df_extended_cases], axis=1) #extend_df
 
-    df_train_cases.to_csv(get_output_dir() + r"/train_cases.csv", index=False)
-    return df_train_cases
+    df_extended_cases.to_csv(get_output_dir() + r"/train_cases.csv", index=False)
+    return df_extended_cases
 
-def weight_learning(df_train_cases):
+def weight_learning(df_extended_cases):
     '''
                Description: Takes a DataFrame and computes RelifF algorithm on it
 
@@ -111,10 +158,10 @@ def weight_learning(df_train_cases):
                Caveats:
            '''
     column_to_drop = 'CaseNumber'  # Replace with the name of the column you want to drop
-    if column_to_drop in df_train_cases.columns:
-        df_train_cases = df_train_cases.drop(columns=[column_to_drop])
-    rff = ReliefF(n_features_to_select=len(df_train_cases.columns), n_neighbors=3)
-    rff.fit(df_train_cases.drop("Decision", axis=1).values, df_train_cases["Decision"].values)
+    if column_to_drop in df_extended_cases.columns:
+        df_extended_cases = df_extended_cases.drop(columns=[column_to_drop])
+    rff = ReliefF(n_features_to_select=len(df_extended_cases.columns), n_neighbors=3)
+    rff.fit(df_extended_cases.drop("Decision", axis=1).values, df_extended_cases["Decision"].values)
     return  rff.feature_importances_
 
 def weighted_distance(sample_x, sample_y, feature_weights):
@@ -159,23 +206,16 @@ def retrieval(X_test, y_test, X_train, y_train, weights, k=1, threshold=10):
     X_train = X_train.values
     X_test = X_test.values[0]
     for i, cc in enumerate(X_train):
-        # print(X_test)
-        # print(cc)
         local_sim = local_similarity(X_test, cc, feature_type)
-        # print(local_sim)
-        # print(weights)
-        # print(weights * local_sim)
-        # print((weights * local_sim)**2)
-        # print(sum((weights * local_sim) ** 2))
-        # print(np.sqrt(sum((weights * local_sim) ** 2)))
+
 
     # Calculate the global similarity as the square root of the sum of squared values
         global_sim_val =  np.sqrt(sum((weights * local_sim) ** 2))
         global_sim.append(global_sim_val)
-    # print(global_sim)
+    
     y_pred = y_train[global_sim.index(max(global_sim))]
     x_pred = X_train_df.iloc[global_sim.index(max(global_sim))]
-    # print(max(global_sim), y_pred)
+            
     if y_pred == y_test:
         dec = 1
     else:
--- components/case_formation/acf/run.py
+++ components/case_formation/acf/run.py
@@ -1,93 +1,73 @@
-# for relative imports until we run this as a package
-import sys
-from typing import Any
-import pandas as pd
 import os
-sys.path.append('.')
-import yaml
-from domain.internal import Probe, State, Decision, KDMAs, KDMA, Scenario, DecisionMetric
-from components import DecisionAnalyzer
-from components.decision_analyzer import *
-# there will probably be a default here. Also, we will probably just call Decision Analyzer
-# python imports are weird
-from components.decision_analyzer.event_based_diagnosis.ebd_analyzer import EventBasedDiagnosisAnalyzer
-from components.decision_selector.cbr.cbr_decision_selector import CBRDecisionSelector, Case
-from components.hra import hra
-from components.hra import *
-from components.case_formation.acf.argument_case_base import *
-
+import pickle
+from argument_case_base import *
 from create_argument import *
+data_dir = get_data_dir()
 
+# initialize the dataframes and create the argument case base
 df_partial_cases = partial_cases()
 df_elab_cases = elab_cases(df_partial_cases)
-df_train_cases = case_expansion(df_elab_cases)
-feature_weights = weight_learning(df_train_cases)
+df_extended_cases = case_expansion(df_elab_cases)
+feature_weights = weight_learning(df_extended_cases)
 print("learned feature weights: ", feature_weights)
-df_argument_case_base = create_argument_case(df_train_cases, feature_weights)
+df_argument_case_base = create_argument_case(df_extended_cases, feature_weights)
 df_argument_label = create_argument(df_argument_case_base)
 alignment_score(df_argument_label)
 
-# just to run it from this location for now
-current_dir = os.path.dirname(os.path.realpath(__file__)) 
-data_dir = os.path.join(current_dir, 'data')
-output_dir = os.path.join(current_dir, 'output')
+print("argument case base: ", df_argument_case_base)
 
-mvp1_df = pd.read_csv(os.path.join(data_dir, 'argument_case_base.csv'))
-mvp2_df = pd.read_csv(os.path.join(data_dir, 'mvp2.csv'))
+# put the argument case base into a list of probes for analysis
+argumentCaseProbes = ArgumentCaseProbe(df_argument_case_base).create_argument_case_probes()
 
-# of we jave time we can join these two files but ingestor will do this for us
-os.path.join(data_dir,'scenario.yaml')
-labels = yaml.load(open(os.path.join(data_dir,'scenario.yaml'), 'r'), Loader=yaml.Loader)
-
-scenario = Scenario(id_=labels['id'], state=labels['state'])
-
-class ArgumentCaseProbe:
-    
-    def __init__(self, data):
-        self.data = data        
-        #print(self.data.columns)
-        self.probes = []
-
-    # we can replace this with ingesting the data as internal    
-    # create the mvp2 probes in the format of the internal Probe
-    def create_mvp2_probes(self):
-        probes = []
-        for index, row in self.data.iterrows():
-            # Probe parameters: id_, state, prompt            
-            probe = Probe(id_=row['session_id'], state=row['probe_id'], prompt=row['probe_id'])
-            
-            kdmas_row = str(row['mission']) + " " +  str(row['denial']) + " " +  str(row['risktol']) + str(row['timeurg'])
-            probe.decisions = [Decision(id_=1, value=row['choice'], kdmas=kdmas_row,)]
-            probes.append(probe)
-        return probes
-
-    def create_mvp1_probes(self):
-        probes = []
-        for index, row in self.data.iterrows():
-            # Probe parameters: id_, state, prompt            
-            probe = Probe(id_=row['CaseNumber'], state=row['Background'], prompt=row['Scenario'])
-            kdmas_row = str(row['Mission']) + " " +  str(row['Risk aversion'])
-            probe.decisions = [Decision(id_=row['CaseNumber'], value=row['Decision'], kdmas=kdmas_row,)]
-            probes.append(probe)
-        return probes
-                
-
-
-# create a list of probes from the MVP1 data
-mvp1_probes = ArgumentCaseProbe(mvp1_df).create_mvp1_probes()
-
-#create a list of probes from the MVP2 data
-mvp_2_probes = ArgumentCaseProbe(mvp2_df).create_mvp2_probes()
-
-#initialize the analyzers
+# initialize the analyzers
+decision_analyzer = DecisionAnalyzer() # not implemented but will probably be a wrapper for the other analyzers
 generic_analyzer = BaselineDecisionAnalyzer()
 monte_carlo_analyzer = MonteCarloAnalyzer()
 #event_based_analyzer = EventBasedDiagnosisAnalyzer() # I get an error
 
+# hra analyzer is a function that takes a json file and returns a dictionary of decisions
 hra_object = hra.HRA()
 hra_analyzer = hra_object.hra_decision_analytics
-# here are other possible hra response types
+# see other possible hra response types below
 a = hra_object.take_the_best(os.path.join(data_dir, "scene_one_treatment.json"))
+k = hra_object.tallying(os.path.join(data_dir, "scene2.json"), 4, 0)
+
+# this is just a placehoder for the scenario
+scenario = Scenario(id_=labels['id'], state=labels['state'])
+
+# here we get the analysis for each probe which is a dictionary of decision metrics
+# these decision metrics can be used to augment the case base with the DecisionMetrics returned
+cases = []
+for probe in argumentCaseProbes:
+    # response is a decision dictionary
+   
+    metrics = {}
+
+    # response is a DecisionMetric dictionary object
+    metrics['generic'] = generic_analyzer.analyze(scenario, probe)
+    metrics['monte_carlo'] = monte_carlo_analyzer.analyze(scenario, probe)    
+    #meterics['event_based'] = event_based_analyzer.analyze(scenario, probe)
+
+    # response is a dictionary of decisions   
+    metrics['hra'] = hra_analyzer(os.path.join(data_dir, 'scene2.json'),0)
+    
+    # we probably only get one set of decision metrics when fully implemented
+    
+    case = Case(scenario, probe, metrics)
+    cases.append(case)
+
+print('added {} cases with Decision Metrics to the case base'.format(len(cases)))
+# do something with the new cases (add them back into a case base)
+pickle.dump(cases, open(get_output_dir() + '/out_case_base.p', 'wb'))
+
+
+#
+#
+#
+#
+#
+#
+# other hra_analyzer response types
 b = hra_object.take_the_best(os.path.join(data_dir, "scene_objective_best.json"))
 c = hra_object.take_the_best(os.path.join(data_dir,"scene_no_preference.json"))
 d = hra_object.exhaustive(os.path.join(data_dir, "scene_one_treatment.json"))
@@ -97,64 +77,3 @@ g = hra_object.tallying(os.path.join(data_dir, "scene_one_treatment.json"), 1)
 h = hra_object.tallying(os.path.join(data_dir, "scene_objective_best.json"), 3, 0)
 i = hra_object.tallying(os.path.join(data_dir, "scene_no_preference.json"), 3)
 j = hra_object.tallying(os.path.join(data_dir, "scene2.json"), 1, 0)
-k = hra_object.tallying(os.path.join(data_dir, "scene2.json"), 4, 0)
-
-    
-
-# here we get the analysis for each probe which is a dictionary of decision metrics
-# these decision metrics can be used to build the argument case with the weights learned as justifications
-# for the SoarTech data, we already have decisions, so this means something different than if we select a decision
-for probe in mvp_2_probes:
-    g = generic_analyzer.analyze(scenario, probe)
-    m = monte_carlo_analyzer.analyze(scenario, probe)
-    print(m)
-    h = hra_analyzer(os.path.join(data_dir, 'scene2.json'),0)
-    
-    
-    #h = hra_analyzer(scenario, probe)
-   #e =event_based_analyzer.analyze(None, probe) I get an error
-
-
-# now for probes that do not have a decision
-# we can use the decision selector to select a decision
-# send the probes to decision selector
-# the decision selector will return a decision
-
-
-#    learn the weights. This is going to be the case base for the decision selector
-dfpc = partial_cases()
-dfec = elab_cases(dfpc)
-dfc = case_expansion(dfec)
-feature_weights = weight_learning(dfc)
-print("learned feature weights: ", feature_weights)
-cases = create_argument_case(dfc, feature_weights)
-
-#create a case base from the probes
-case_base = []
-
-unknown_probes = []
-
-for probe in unknown_probes:
-    target_kdmas = KDMAs([KDMA('mission', 0.5), KDMA('denial', 0.5)])
-    
-    selected_decision = None#CBRDecisionSelector.select(c,'scenario', probe, target_kdmas)
-    # now we can use the decision to get the decision metrics using the weights we learned above
-    new_probe = Probe(id_=probe.id_, state=probe.state, prompt=probe.prompt, decisions=[selected_decision])
-    g = generic_analyzer.analyze(None, probe)
-    m = monte_carlo_analyzer.analyze(None, probe)
-    #e =event_based_analyzer.analyze(None, probe) I get an error
-    #cases.append(Case(None, new_probe, selected_decision, g, m))
-
- 
-# the decision will be the decision selected by the decision selector
-# the decision selector will be called from the runner
-# the runner will be called from main
-
-    
-    
-
-    
-
-
-
-
