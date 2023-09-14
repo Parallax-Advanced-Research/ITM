8da312064c59ac02976dfebac0dd32e7a63884c9
cr625 <cr625@drexel.edu>
2023-09-14 04:21:35 -0400

    update comments

--- components/case_formation/acf/run.py
+++ components/case_formation/acf/run.py
@@ -10,7 +10,8 @@ df_elab_cases = elab_cases(df_partial_cases)
 df_extended_cases = case_expansion(df_elab_cases)
 # internal note: the df_extended_cases is the argument case base for which we would get the decision metrics
 # but right now the weight learning doesn't handle the added decision metrics as features so we skip ahead to have an output
-# argument_case_probes = ArgumentCaseProbe(df_extended_cases).create_argument_case_probes()
+# argument_case_probes = ArgumentCaseProbe(df_extended_cases).create_argument_case_probes() --> extended cases plus decision metrics
+# feature_weights = weight_learning(argument_case_probes)
 feature_weights = weight_learning(df_extended_cases)
 print("learned feature weights: ", feature_weights)
 df_argument_case_base = create_argument_case(df_extended_cases, feature_weights)
