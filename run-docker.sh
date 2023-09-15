2a1f78fc94794ed35f28233b8bf21d13b08de84c
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2023-09-15 17:13:15 -0400

    Added support for building and running docker images.

---
+++ run-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -z "$TA3_ENDPOINT" ]
+   then TA3_ENDPOINT=$1
+fi
+if [ -z "$TA3_ENDPOINT" ]
+   then TA3_ENDPOINT="127.0.0.1:8080"
+fi
+
+docker run --env PYTHONPATH=. -it tad python3 scripts/tad_tester.py --endpoint $TA3_ENDPOINT
