e182d520ce01ddbfa7d8645ad6f940eb6dee1122
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2023-09-17 16:45:21 -0400

    Incorporated args in driver. Standardized package usage.

--- runner/ta3_driver.py
+++ runner/ta3_driver.py
@@ -3,16 +3,16 @@ from components.decision_selector.default import HumanDecisionSelector
 from components.decision_selector.sept_cbr import CSVDecisionSelector
 from components.elaborator.default import TA3Elaborator
 from components.decision_analyzer.default import BaselineDecisionAnalyzer
-from components.decision_analyzer.monte_carlo import monte_carlo_analyzer as mca
+from components.decision_analyzer.monte_carlo import MonteCarloAnalyzer
 from components.decision_analyzer.event_based_diagnosis import EventBasedDiagnosisAnalyzer
-
+from components.decision_analyzer.bayesian_network import BayesNetDiagnosisAnalyzer
 from components.decision_analyzer.heuristic_rule_analysis import HeuristicRuleAnalyzer
 from .driver import Driver
 
 
 class TA3Driver(Driver):
-    def __init__(self, human = False):
-        if human:
+    def __init__(self, args):
+        if args.human:
             selector = HumanDecisionSelector()
         else:
             selector = CSVDecisionSelector("data/sept/case_base.csv")
@@ -23,7 +23,7 @@ class TA3Driver(Driver):
             analyzer1 = BaselineDecisionAnalyzer()
         analyzer2 = HeuristicRuleAnalyzer()
         analyzer3 = BayesNetDiagnosisAnalyzer()
-        analyzer4 = mca.MonteCarloAnalyzer(max_rollouts=10000, max_depth=2)
+        analyzer4 = MonteCarloAnalyzer(max_rollouts=10000, max_depth=2)
 
         super().__init__(elaborator, selector, [analyzer1, analyzer2, analyzer3,analyzer4])
 
