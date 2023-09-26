4fb4995d9fc8e02d17c91e9b28cba2c4e56057b1
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2023-09-26 18:34:53 -0400

    Added aligned default for variant parameter.

--- scripts/tad_tester.py
+++ scripts/tad_tester.py
@@ -97,7 +97,7 @@ def main():
     parser.add_argument('--keds', action=argparse.BooleanOptionalAction, default=False, help="Uses KDMA Estimation Decision Selector for decision selection (default is CSVDecisionSelector)")
     parser.add_argument('--rollouts', type=int, default=10000, help="Monte Carlo rollouts to perform")
     parser.add_argument('--endpoint', type=str, help="The URL of the TA3 api", default=None)
-    parser.add_argument('--variant', type=str, help="TAD variant", default=None)
+    parser.add_argument('--variant', type=str, help="TAD variant", default="aligned")
     args = parser.parse_args()
 
     test_endpoint(args)
