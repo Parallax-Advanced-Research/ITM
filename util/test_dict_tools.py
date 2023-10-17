e391b01fdfcaf98476726de1bc42657f2ac9ac9d
Ray Kulhanek <ray.kulhanek@parallaxresearch.org>
2023-10-17 12:09:40 -0400

    adding more tests, support for some testing frameworks

--- util/test_dict_tools.py
+++ util/test_dict_tools.py
@@ -1,8 +1,8 @@
 # TODO: Right now, this just tests Dict_No_Overwrite. It should also test the functions that
 # already existed.
 
-import sys, inspect
-from dict_tools import Dict_No_Overwrite
+import inspect, pytest
+from .dict_tools import Dict_No_Overwrite
 
 def f(a: Dict_No_Overwrite[str,int]) -> None:
 	global f1_lineno
@@ -15,8 +15,8 @@ def g_aux(a: Dict_No_Overwrite[str,int]) -> None:
 	a['g0'] = 20
 	g0_lineno = inspect.stack()[0].frame.f_lineno - 1
 	a['g1'] = 21
-	a['f1'] = 21 # error
-	a['g2'] = 22 # won't get written
+	a['f1'] = 21 # error if f called already
+	a['g2'] = 22 # won't get written if...
 
 def g(a: Dict_No_Overwrite[str,int]) -> None:
 	g_aux(a)
@@ -24,77 +24,97 @@ def g(a: Dict_No_Overwrite[str,int]) -> None:
 def h(a: Dict_No_Overwrite[str,int]) -> None:
 	a['h0'] = 30
 	a['h1'] = 31
-	a['g0'] = 30 # error
-	a['h2'] = 32 # won't get written
+	a['g0'] = 30 # error if g called already
+	a['h2'] = 32 # won't get written if...
 
-def main() -> None:
+def test_setitem_depth2() -> None:
 	a = Dict_No_Overwrite[str,int]()
 	f(a)
-
-	raised = False
+	
 	try:
 		g(a)
 	except Exception as e:
-		raised = True
 		# not testing specific message, since it depends on directory
 		assert 'Key `f1` already set by ' in str(e)
 		assert f'test_dict_tools.py:{f1_lineno}' in str(e)
-	assert raised
+		assert a == { 'f0': 10, 'f1': 11, 'g0': 20, 'g1': 21 }
+		return
 
-	raised = False
+	assert False, "Exception wasn't raised"
+
+def test_setitem_depth1() -> None:
+	# differs from test_setitem by the stack depth at which things happen:
+	# we want to make sure the error message is right in both cases.
+	a = Dict_No_Overwrite[str,int]()
+	g(a)
+	
 	try:
 		h(a)
 	except Exception as e:
-		raised = True
 		assert 'Key `g0` already set by ' in str(e)
 		assert f'test_dict_tools.py:{g0_lineno}' in str(e)
-	assert raised
+		assert a == { 'f1': 21, 'g0': 20, 'g1': 21, 'g2': 22, 'h0': 30, 'h1': 31 }
+		return
+	assert False, "Exception wasn't raised"
 
-	assert a == { 'f0': 10, 'f1': 11, 'g0': 20, 'g1': 21, 'h0': 30, 'h1': 31 }
-
+def test_update() -> None:
+	a = Dict_No_Overwrite[str,int]()
+	f(a)
 	good_update = { 'i0': 40, 'i1': 41 }
 	bad_update = { 'f1': 41, 'i2': 42 }
-	raised = False
+	
 	try:
 		a.update(good_update)
 		a.update(bad_update)
 	except Exception as e:
-		raised = True
 		assert 'Key `f1` already set by ' in str(e)
 		assert f'test_dict_tools.py:{f1_lineno}' in str(e)
-	assert raised
-	assert a == { 'f0': 10, 'f1': 11, 'g0': 20, 'g1': 21, 'h0': 30, 'h1': 31, 'i0': 40, 'i1': 41 }
+		assert a == { 'f0': 10, 'f1': 11, 'i0': 40, 'i1': 41 }
+		return
+	assert False, "Exception wasn't raised"
 
-	good_update = { 'j0': 50, 'j1': 51 }
-	bad_update = { 'f1': 51, 'j2': 52 }
-	raised = False
+def test_or() -> None:
+	a = Dict_No_Overwrite[str,int]()
+	f(a)
+	good_update = { 'i0': 40, 'i1': 41 }
+	bad_update = { 'f1': 41, 'i2': 42 }
+	
 	try:
 		c = a | good_update
 		c = c | bad_update
 	except Exception as e:
-		raised = True
 		assert 'Key `f1` already set by ' in str(e)
 		assert f'test_dict_tools.py:{f1_lineno}' in str(e)
-	assert raised
-	assert c == { 'f0': 10, 'f1': 11, 'g0': 20, 'g1': 21, 'h0': 30, 'h1': 31, 'i0': 40, 'i1': 41, 'j0': 50, 'j1': 51}
+		assert c == { 'f0': 10, 'f1': 11, 'i0': 40, 'i1': 41 }
+		return
+	assert False, "Exception wasn't raised"
 
-	good_update = { 'k0': 60, 'k1': 61 }
-	bad_update = { 'f1': 61, 'k2': 62 }
-	raised = False
+def test_ior() -> None:
+	a = Dict_No_Overwrite[str,int]()
+	f(a)
+	good_update = { 'i0': 40, 'i1': 41 }
+	bad_update = { 'f1': 41, 'i2': 42 }
+	
 	try:
 		a |= good_update
 		a |= bad_update
 	except Exception as e:
-		raised = True
 		assert 'Key `f1` already set by ' in str(e)
 		assert f'test_dict_tools.py:{f1_lineno}' in str(e)
-	assert raised
-	assert a == { 'f0': 10, 'f1': 11, 'g0': 20, 'g1': 21, 'h0': 30, 'h1': 31, 'i0': 40, 'i1': 41, 'k0': 60, 'k1': 61}
-	
+		assert a == { 'f0': 10, 'f1': 11, 'i0': 40, 'i1': 41 }
+		return
+	assert False, "Exception wasn't raised"
+
+def test_update_overwrite() -> None:
+	a = Dict_No_Overwrite[str,int]()
+	f(a)
+	bad_update = { 'f1': 41, 'i2': 42 }
 	a.update_overwrite(bad_update)
-	assert a == { 'f0': 10, 'f1': 61, 'g0': 20, 'g1': 21, 'h0': 30, 'h1': 31, 'i0': 40, 'i1': 41, 'k0': 60, 'k1': 61, 'k2': 62}
+	assert a == { 'f0': 10, 'f1': 41, 'i2': 42 }
 
+def test_overwrite() -> None:
+	a = Dict_No_Overwrite[str,int]()
+	f(a)
 	a.overwrite('f0', 60)
-	assert a == { 'f0': 60, 'f1': 61, 'g0': 20, 'g1': 21, 'h0': 30, 'h1': 31, 'i0': 40, 'i1': 41, 'k0': 60, 'k1': 61, 'k2': 62}
+	assert a == { 'f0': 60, 'f1': 11 }
 
-main()
