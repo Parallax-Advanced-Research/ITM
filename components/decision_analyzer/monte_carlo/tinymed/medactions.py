ac89353c0e048cc7a81960451f28a9ea8095e1ed
jt <jt.turner@knexusresearch.com>
2023-10-19 13:28:50 -0400

    refactoring done for creating seperate medical oracle object

--- components/decision_analyzer/monte_carlo/tinymed/medactions.py
+++ components/decision_analyzer/monte_carlo/tinymed/medactions.py
@@ -1,10 +1,9 @@
 import random
 
 from .tinymed_state import TinymedAction, TinymedState
-from .tinymed_enums import Casualty, Supplies, Actions, Locations, Tags, Injury, Injuries, MentalStates, \
-    BreathingDescriptions, Demographics, Vitals
+from .tinymed_enums import Casualty, Supplies, Actions, Locations, Tags, Injury, Injuries
+from .oracle import MedicalOracle
 import typing
-
 
 
 def treatment_supply_match(action: TinymedAction) -> bool:
@@ -21,8 +20,6 @@ def treatment_supply_match(action: TinymedAction) -> bool:
     return True
 
 
-
-
 def supply_injury_match(supply: str, injury: str) -> bool:
     if supply in [Supplies.PRESSURE_BANDAGE.value, Supplies.HEMOSTATIC_GAUZE.value, Supplies.TOURNIQUET.value]:
         if injury in [Injuries.ASTHMATIC.value, Injuries.CHEST_COLLAPSE.value]:
@@ -33,8 +30,6 @@ def supply_injury_match(supply: str, injury: str) -> bool:
             return True
         return False
     return True
-
-
 
 
 def apply_generic_treatment(casualty: Casualty, supplies: dict[str, int],
@@ -127,36 +122,6 @@ def default_action(casualties: list[Casualty], supplies: dict[str, int],
     return [same_state]
 
 
-# def apply_casualtytag_action(casualties: list[Casualty], supplies: dict[str, int],
-#                             action: TinymedAction, rng: random.Random, start_time: float) -> list[TinymedState]:
-#     c1 = find_casualty(action, casualties)
-#     time_taken = rng.choice(MedicalOracle.TIME_TAKEN[action.action])
-#     c1.tag = action.tag
-#     for c in casualties:
-#         casualty_injuries: list[Injury] = c.injuries
-#         for ci in casualty_injuries:
-#             get_update_function_for_injury(ci.name)(ci, time_taken)
-#     new_state = TinymedState(casualties=casualties, supplies=supplies, time=start_time + time_taken)
-#     return [new_state]
-#
-#
-# def apply_singlecaualty_action(casualties: list[Casualty], supplies: dict[str, int],
-#                             action: TinymedAction, rng: random.Random, start_time: float) -> list[TinymedState]:
-#     time_taken = rng.choice(MedicalOracle.TIME_TAKEN[action.action])
-#     for c in casualties:
-#         casualty_injuries: list[Injury] = c.injuries
-#         for ci in casualty_injuries:
-#             get_update_function_for_injury(ci.name)(ci, time_taken)
-#     new_state = TinymedState(casualties=casualties, supplies=supplies, time=start_time + time_taken)
-#     return [new_state]
-#
-#
-# def default_action(casualties: list[Casualty], supplies: dict[str, int],
-#                             action: TinymedAction, rng: random.Random, start_time: float) -> list[TinymedState]:
-#     same_state = TinymedState(casualties, supplies, time=start_time)
-#     return [same_state]
-
-
 def get_treatment_actions(casualties: list[Casualty], supplies: list[str]) -> list[tuple]:
     treatments: list[tuple] = []
     locations: list[str] = [loc.value for loc in Locations]
@@ -210,7 +175,6 @@ def get_possible_actions(casualties: list[Casualty], supplies: list[str]) -> lis
     move_to_evac_actions = get_action_all(casualties, Actions.MOVE_TO_EVAC.value)
     sitrep_actions = get_action_all(casualties, Actions.SITREP.value)
     tag_actions = get_tag_options(casualties)
-    # more get_blah_actions_here later
 
     possible_action_tuples.extend(treatment_actions)
     possible_action_tuples.extend(check_all_actions)
@@ -304,70 +268,6 @@ def get_injury_update_time(injury_name: str):
         return MedicalOracle.INJURY_UPDATE_TIMES[Injuries.FOREHEAD_SCRAPE.value]  # Assume not serious
 
 
-def get_TMNT_demo_casualties() -> list[Casualty]:
-    wrist_bump = Injury(name=Injuries.LACERATION.value, location=Locations.LEFT_WRIST.value, severity=1.0)
-    minor_cut = Injury(name=Injuries.LACERATION.value, location=Locations.RIGHT_BICEP.value, severity=3.0)
-    moder_cut = Injury(name=Injuries.LACERATION.value, location=Locations.LEFT_SIDE.value, severity=5.0)
-    major_cut = Injury(name=Injuries.LACERATION.value, location=Locations.LEFT_THIGH.value, severity=7.0)
-    collapsed_lung = Injury(name=Injuries.CHEST_COLLAPSE.value, location=Locations.UNSPECIFIED.value,  severity=8.0)
-
-    raphael_vitals = Vitals(conscious=True, mental_status=MentalStates.DANDY.value,
-                            breathing=BreathingDescriptions.NORMAL.value, hrpmin=49)
-    michelangelo_vitals = Vitals(conscious=True, mental_status=MentalStates.FINE.value,
-                                 breathing=BreathingDescriptions.NORMAL.value, hrpmin=68)
-    donatello_vitals = Vitals(conscious=True, mental_status=MentalStates.FINE.value,
-                              breathing=BreathingDescriptions.HEAVY.value, hrpmin=81)
-    leonardo_vitals = Vitals(conscious=True, mental_status=MentalStates.PANICKED.value,
-                             breathing=BreathingDescriptions.COLLAPSED.value, hrpmin=50)
-    casualties = [
-        Casualty('raphael', 'raphael has a bump on his left wrist', name='raphael',
-                       relationship='same unit',
-                       demographics=Demographics(age=15, sex='M', rank='muscle'),
-                       injuries=[wrist_bump],
-                       vitals=raphael_vitals,
-                       complete_vitals=raphael_vitals,
-                       assessed=False,
-                       tag="tag"),
-        Casualty('michelangelo', 'michelangelo has a minor laceration on his right bicep',
-                       name='michelangelo',
-                       relationship='same unit',
-                       demographics=Demographics(age=15, sex='M', rank='the wild one'),
-                       injuries=[minor_cut],
-                       vitals=michelangelo_vitals,
-                       complete_vitals=michelangelo_vitals,
-                       assessed=False,
-                       tag="tag"),
-        Casualty('donatello', 'donatello has a major cut on his left thigh',
-                       name='donatello',
-                       relationship='same unit',
-                       demographics=Demographics(age=15, sex='M', rank='the brains'),
-                       injuries=[major_cut],
-                       vitals=donatello_vitals,
-                       complete_vitals=donatello_vitals,
-                       assessed=False,
-                       tag="tag"),
-        Casualty('leonardo', 'leonardo is unable to breathe, and has moderate cut across the left side of abdomen',
-                       name='leonardo',
-                       relationship='same unit',
-                       demographics=Demographics(age=15, sex='M', rank='the leader'),
-                       injuries=[moder_cut, collapsed_lung],
-                       vitals=leonardo_vitals,
-                       complete_vitals=leonardo_vitals,
-                       assessed=False,
-                       tag="tag"),
-    ]
-    return casualties
-
-
-def get_TMNT_supplies() -> dict[str, int]:
-    supplies = {
-        Supplies.TOURNIQUET.value: 3,
-        Supplies.PRESSURE_BANDAGE.value: 2,
-        Supplies.HEMOSTATIC_GAUZE.value: 2,
-        Supplies.DECOMPRESSION_NEEDLE.value: 2,
-        Supplies.NASOPHARYNGEAL_AIRWAY.value: 3
-    }
-    return supplies
 resolve_action = typing.Callable[[list[Casualty], dict[str, int], TinymedAction, random.Random, int], list[TinymedState]]
 resolve_injury = typing.Callable[[Casualty, dict[str, int], TinymedAction, random.Random], float]
 update_injury = typing.Callable[[Injury, float], None]
@@ -405,92 +305,3 @@ action_map: typing.Mapping[str, resolve_action] = {
     Actions.END_SCENARIO.value: apply_zeroornone_action
 
 }
-
-
-# def get_update_function_for_injury(injury_name: str):
-#     return update_generic_injury
-
-
-def get_simple_casualties():
-    bicep_tear = Injury(name=Injuries.LACERATION.value, location=Locations.LEFT_BICEP.value, severity=4.0, treated=False)
-    jt_vitals = Vitals(conscious=True, mental_status=MentalStates.DANDY.value,
-                       breathing=BreathingDescriptions.NORMAL.value, hrpmin=69)
-    casualties = [
-        Casualty('JT', 'JT tore his bicep', name='JT',
-                       relationship='himself',
-                       demographics=Demographics(age=33, sex='M', rank='director of social media'),
-                       injuries=[bicep_tear],
-                       vitals=jt_vitals,
-                       complete_vitals=jt_vitals,
-                       assessed=False,
-                       tag="tag")]
-    return casualties
-
-
-def get_simple_supplies() -> dict[str, int]:
-    supplies = {
-        Supplies.TOURNIQUET.value: 0,
-        Supplies.PRESSURE_BANDAGE.value: 3,
-        Supplies.HEMOSTATIC_GAUZE.value: 0,
-        Supplies.DECOMPRESSION_NEEDLE.value: 0,
-        Supplies.NASOPHARYNGEAL_AIRWAY.value: 0
-    }
-    return supplies
-
-
-class MedicalOracle:
-    FAILURE_CHANCE = {
-        Supplies.PRESSURE_BANDAGE.value: .16,
-        Supplies.HEMOSTATIC_GAUZE.value: .25,
-        Supplies.TOURNIQUET.value: 0,
-        Supplies.DECOMPRESSION_NEEDLE.value: .10,
-        Supplies.NASOPHARYNGEAL_AIRWAY.value: .05
-    }
-
-    TIME_TAKEN = {
-        Supplies.PRESSURE_BANDAGE.value: [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 3.0, 4.5],
-        Supplies.HEMOSTATIC_GAUZE.value: [1.0, 1.5],
-        Supplies.TOURNIQUET.value: [3.0, 3.0, 3.0, 6.0],
-        Supplies.DECOMPRESSION_NEEDLE.value: [5.0, 7.0],
-        Supplies.NASOPHARYNGEAL_AIRWAY.value: [2.0],
-        Actions.CHECK_PULSE.value: [1.0],
-        Actions.CHECK_RESPIRATION.value: [1.0],
-        Actions.CHECK_ALL_VITALS.value: [1.5],
-        Actions.SITREP.value: [1.0],
-        Actions.TAG_CASUALTY.value: [1.0],
-        Actions.MOVE_TO_EVAC.value: [1.0],
-        Actions.DIRECT_MOBILE_CASUALTY.value: [1.0],
-        Actions.END_SCENARIO.value: [0.0]
-    }
-
-    SUCCESSFUL_SEVERITY = {
-        Supplies.PRESSURE_BANDAGE.value: 1.0,
-        Supplies.HEMOSTATIC_GAUZE.value: 1.0,
-        Supplies.TOURNIQUET.value: 3.0,
-        Supplies.DECOMPRESSION_NEEDLE.value: 2.0,
-        Supplies.NASOPHARYNGEAL_AIRWAY.value: 2.0
-    }
-
-    INJURY_UPDATE_TIMES = {
-        Injuries.LACERATION.value: .49,
-        Injuries.BURN.value: .42,
-        Injuries.PUNCTURE.value: .36,
-        Injuries.SHRAPNEL.value: .16,
-        Injuries.AMPUTATION.value: .64,
-        Injuries.ASTHMATIC.value: .04,
-        Injuries.CHEST_COLLAPSE.value: .64,
-        Injuries.EAR_BLEED.value: .03,
-        Injuries.FOREHEAD_SCRAPE.value: .03
-    }
-
-    TREATABLE_AREAS = {
-        Supplies.TOURNIQUET.value: [Locations.UNSPECIFIED.value, Locations.LEFT_SIDE.value, Locations.LEFT_NECK.value,
-                               Locations.LEFT_CHEST.value, Locations.LEFT_SHOULDER.value, Locations.LEFT_FACE.value,
-                               Locations.LEFT_STOMACH.value, Locations.RIGHT_SIDE.value, Locations.RIGHT_NECK.value,
-                               Locations.RIGHT_CHEST.value, Locations.RIGHT_SHOULDER.value, Locations.RIGHT_FACE.value,
-                               Locations.RIGHT_STOMACH.value],
-        Supplies.DECOMPRESSION_NEEDLE.value: [Locations.LEFT_CHEST.value, Locations.LEFT_SIDE.value, Locations.LEFT_STOMACH.value,
-                               Locations.RIGHT_CHEST.value, Locations.RIGHT_SIDE.value, Locations.RIGHT_STOMACH.value,
-                               Locations.UNSPECIFIED.value],
-        Supplies.NASOPHARYNGEAL_AIRWAY.value: [Locations.LEFT_FACE.value, Locations.RIGHT_FACE.value, Locations.LEFT_NECK.value,
-                                   Locations.RIGHT_NECK.value, Locations.UNSPECIFIED.value]}
---
+++ components/decision_analyzer/monte_carlo/tinymed/oracle.py
@@ -0,0 +1,58 @@
+from .tinymed_enums import Supplies, Actions, Injuries, Locations
+
+class MedicalOracle:
+    FAILURE_CHANCE = {
+        Supplies.PRESSURE_BANDAGE.value: .16,
+        Supplies.HEMOSTATIC_GAUZE.value: .25,
+        Supplies.TOURNIQUET.value: 0,
+        Supplies.DECOMPRESSION_NEEDLE.value: .10,
+        Supplies.NASOPHARYNGEAL_AIRWAY.value: .05
+    }
+
+    TIME_TAKEN = {
+        Supplies.PRESSURE_BANDAGE.value: [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 3.0, 4.5],
+        Supplies.HEMOSTATIC_GAUZE.value: [1.0, 1.5],
+        Supplies.TOURNIQUET.value: [3.0, 3.0, 3.0, 6.0],
+        Supplies.DECOMPRESSION_NEEDLE.value: [5.0, 7.0],
+        Supplies.NASOPHARYNGEAL_AIRWAY.value: [2.0],
+        Actions.CHECK_PULSE.value: [1.0],
+        Actions.CHECK_RESPIRATION.value: [1.0],
+        Actions.CHECK_ALL_VITALS.value: [1.5],
+        Actions.SITREP.value: [1.0],
+        Actions.TAG_CASUALTY.value: [1.0],
+        Actions.MOVE_TO_EVAC.value: [1.0],
+        Actions.DIRECT_MOBILE_CASUALTY.value: [1.0],
+        Actions.END_SCENARIO.value: [0.0]
+    }
+
+    SUCCESSFUL_SEVERITY = {
+        Supplies.PRESSURE_BANDAGE.value: 1.0,
+        Supplies.HEMOSTATIC_GAUZE.value: 1.0,
+        Supplies.TOURNIQUET.value: 3.0,
+        Supplies.DECOMPRESSION_NEEDLE.value: 2.0,
+        Supplies.NASOPHARYNGEAL_AIRWAY.value: 2.0
+    }
+
+    INJURY_UPDATE_TIMES = {
+        Injuries.LACERATION.value: .49,
+        Injuries.BURN.value: .42,
+        Injuries.PUNCTURE.value: .36,
+        Injuries.SHRAPNEL.value: .16,
+        Injuries.AMPUTATION.value: .64,
+        Injuries.ASTHMATIC.value: .04,
+        Injuries.CHEST_COLLAPSE.value: .64,
+        Injuries.EAR_BLEED.value: .03,
+        Injuries.FOREHEAD_SCRAPE.value: .03
+    }
+
+    TREATABLE_AREAS = {
+        Supplies.TOURNIQUET.value: [Locations.UNSPECIFIED.value, Locations.LEFT_SIDE.value, Locations.LEFT_NECK.value,
+                               Locations.LEFT_CHEST.value, Locations.LEFT_SHOULDER.value, Locations.LEFT_FACE.value,
+                               Locations.LEFT_STOMACH.value, Locations.RIGHT_SIDE.value, Locations.RIGHT_NECK.value,
+                               Locations.RIGHT_CHEST.value, Locations.RIGHT_SHOULDER.value, Locations.RIGHT_FACE.value,
+                               Locations.RIGHT_STOMACH.value],
+        Supplies.DECOMPRESSION_NEEDLE.value: [Locations.LEFT_CHEST.value, Locations.LEFT_SIDE.value, Locations.LEFT_STOMACH.value,
+                               Locations.RIGHT_CHEST.value, Locations.RIGHT_SIDE.value, Locations.RIGHT_STOMACH.value,
+                               Locations.UNSPECIFIED.value],
+        Supplies.NASOPHARYNGEAL_AIRWAY.value: [Locations.LEFT_FACE.value, Locations.RIGHT_FACE.value, Locations.LEFT_NECK.value,
+                                   Locations.RIGHT_NECK.value, Locations.UNSPECIFIED.value]}
