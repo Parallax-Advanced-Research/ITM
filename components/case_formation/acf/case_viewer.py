0eee6993bbfdadb3d8751fc460d7d84205147a0e
Chris R <70023998+cr625@users.noreply.github.com>
2023-10-29 15:41:20 -0400

    add data sources for cb

---
+++ components/case_formation/acf/case_viewer.py
@@ -0,0 +1,25 @@
+from app import create_app, db
+from app.case.models import Case, CaseBase
+from app.probe.models import Probe, ProbeOption, ProbeResponse
+from app.scenario.models import Scenario, Threat, Casualty, Vitals, Supply
+from . import cli
+
+app = create_app()
+cli.register(app)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "Case": Case,
+        "CaseBase": CaseBase,
+        "Probe": Probe,
+        "ProbeOption": ProbeOption,
+        "ProbeResponse": ProbeResponse,
+        "Scenario": Scenario,
+        "Threat": Threat,
+        "Casualty": Casualty,
+        "Vitals": Vitals,
+        "Supply": Supply,
+    }
