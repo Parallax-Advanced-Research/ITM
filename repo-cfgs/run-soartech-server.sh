0f74e02aeb80e419f4d1da07d1a9eb3e03cb2c7b
Matthew Molineaux <matthew.molineaux@parallaxresearch.org>
2024-01-19 18:08:55 -0500

    Standardized server starting functionality.

--- repo-cfgs/run-soartech-server.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-cd .deprepos/ta1-server-mvp
-source venv/bin/activate
-python -m itm_app > soartech-log.txt 2>&1
-deactivate
--- run-servers.sh
+++ run-servers.sh
@@ -5,7 +5,6 @@ repo-cfgs/update-server.sh itm-evaluation-server
 repo-cfgs/update-server.sh ta1-server-mvp
 repo-cfgs/update-server.sh adept_server
 
-repo-cfgs/run-ta3-server.sh &
-repo-cfgs/run-adept-server.sh &
-repo-cfgs/run-soartech-server.sh &
-
+repo-cfgs/start-server.sh itm-evaluation-server 8080 TA3 -m swagger_server
+repo-cfgs/start-server.sh ta1-server-mvp 8084 Soartech -m itm_app
+repo-cfgs/start-server.sh adept_server 8081 BBN -m openapi_server --port 8081
