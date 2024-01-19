cbb3d8de7fb2d87bc6e1505df58d2e883848ebfc
Matthew Molineaux <matthew.molineaux@parallaxresearch.org>
2024-01-19 11:54:26 -0500

    Fixed update-server to use correct path, check for empty patch.

--- repo-cfgs/update-server.sh
+++ repo-cfgs/update-server.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 cd .deprepos/$1
 git fetch --all
-git checkout `cat ../../repo-cfg/$1-commit-hash`
-git apply-patch ../../repo-cfg/$1.patch
+git checkout `cat ../../repo-cfgs/$1-commit-hash`
+if [ -s diff.txt ]; then
+  git apply-patch ../../repo-cfgs/$1.patch
+fi
