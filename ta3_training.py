a7eff7a8343a073788035d477398872c6ce75540
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2024-02-12 15:55:08 -0500

    Added functionality for running data analysis to create scored csv cases to end of training function.

--- ta3_training.py
+++ ta3_training.py
@@ -1,4 +1,5 @@
 from scripts.shared import parse_default_arguments
+from scripts import analyze_data
 from runner import TA3Driver
 import tad
 
@@ -7,7 +8,6 @@ def main():
     args.training = True
     args.exhaustive = True
     args.keds = False
-    args.kedsd = False
     args.verbose = False
     args.dump = False
     
@@ -18,8 +18,12 @@ def main():
         tad.api_test(args, driver)
         driver.actions_performed = []
         driver.treatments = {}
+    output_training_cases()
     
-
+def output_training_cases():
+    (cases, training_data) = analyze_data.read_training_data()
+    analyze_data.write_kdma_cases_to_csv("temp/kdma_cases.csv", cases, training_data)
+    analyze_data.write_alignment_target_cases_to_csv("temp/alignment_target_cases.csv", cases, training_data)
 
 if __name__ == '__main__':
     main()
