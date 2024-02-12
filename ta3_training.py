d33c0b14aad661ee6f430c1441cfa7dc5fc70c91
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2024-02-12 15:59:07 -0500

    Removed extra argument in call.

--- ta3_training.py
+++ ta3_training.py
@@ -23,7 +23,7 @@ def main():
 def output_training_cases():
     (cases, training_data) = analyze_data.read_training_data()
     analyze_data.write_kdma_cases_to_csv("temp/kdma_cases.csv", cases, training_data)
-    analyze_data.write_alignment_target_cases_to_csv("temp/alignment_target_cases.csv", cases, training_data)
+    analyze_data.write_alignment_target_cases_to_csv("temp/alignment_target_cases.csv", training_data)
 
 if __name__ == '__main__':
     main()
