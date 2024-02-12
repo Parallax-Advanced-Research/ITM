b0d06d43aa297321efef06918beb5badf314baf0
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2024-02-12 15:52:23 -0500

    Added code to create csv based data from recorded feedback and case files. Created separated structure for kdma and alignment target data to acknowledge that these are two independent problems that can be worked separately.

--- scripts/analyze_data.py
+++ scripts/analyze_data.py
@@ -1,6 +1,7 @@
 import json
 from components.decision_selector.exhaustive import exhaustive_selector
 from components.alignment_trainer import kdma_case_base_retainer
+from components.decision_selector.kdma_estimation import write_case_base
 from typing import Any
 
 def read_training_data(case_file: str = exhaustive_selector.CASE_FILE, 
@@ -12,8 +13,11 @@ def read_training_data(case_file: str = exhaustive_selector.CASE_FILE,
         training_data = [json.loads(line) for line in infile]
     return cases, training_data
 
-def find_feedback(actions: list[dict[str, Any]], training_data: list[dict[str, Any]]) -> list[dict[str, Any]]:
-    return [training_record for training_record in training_data if starts_with(training_record["actions"], actions)]
+def find_feedback(actions: list[dict[str, Any]], training_data: list[dict[str, Any]], scenario: str, target: str) -> list[dict[str, Any]]:
+    return [training_record for training_record in training_data 
+                if starts_with(training_record["actions"], actions) 
+                   and training_record["scenario_id"] == scenario 
+                   and training_record["feedback"]["target"] == target]
 
 def starts_with(action_list: list[dict[str, Any]], action_prefix: list[dict[str, any]]) -> bool:
     if len(action_list) < len(action_prefix):
@@ -28,6 +32,7 @@ def starts_with(action_list: list[dict[str, Any]], action_prefix: list[dict[str,
 
 def get_kdma_score_distributions(training_records: list[dict[str, Any]]) -> dict[dict[str, float]]:
     rets = {"score": get_distribution([record["feedback"]["score"] for record in training_records])}
+    rets["count"] = len(training_records)
     for kdma_name in training_records[0]["feedback"]["kdmas"].keys():
         rets[kdma_name] = get_distribution([record["feedback"]["kdmas"][kdma_name] for record in training_records])
     return rets
@@ -35,7 +40,6 @@ def get_kdma_score_distributions(training_records: list[dict[str, Any]]) -> dict
 def get_distribution(sample: list[float]) -> dict[str, float]:
     return {"min": min(sample), 
             "max": max(sample), 
-            "count": len(sample), 
             "average": sum(sample) / len(sample)}
             
 def group_cases(cases: list[dict[str, Any]], example_case: dict[str, Any]) -> list[dict[str, Any]]:
@@ -53,3 +57,54 @@ def input_match(example_case: dict[str, Any], case: dict[str, Any]) -> bool:
         if example_case.get(key, None) != case.get(key, None):
             return False
     return True
+
+def flatten(name, valueDict: dict[str, Any]):
+    ret = {}
+    for (key, value) in valueDict.items():
+        if type(value) is not dict:
+            ret[name + "." + key] = value
+        else:
+            for (subkey, subvalue) in flatten(key, value).items():
+                ret[name + "." + subkey] = subvalue
+
+    return ret
+
+def write_kdma_cases_to_csv(fname: str, cases: list[dict[str, Any]], training_data: list[dict[str, Any]], scenario: str = None, target: str = None):
+    if scenario is None:
+        scenario = training_data[0]["scenario_id"]
+    if target is None:
+        target = training_data[0]["feedback"]["target"]
+    ret_cases = []
+    index = 1
+    for case in {str(case["actions"]):case for case in cases}.values():
+        new_case = dict(case)
+        new_case["index"] = index
+        new_case["action"] = case["actions"][-1]
+        new_case.pop("actions")
+        feedbacks = find_feedback(case["actions"], training_data, scenario, target)
+        if len(feedbacks) == 0:
+            print(f"No feedback for action sequence: {case['actions']}")
+            continue
+        after_feedback = get_kdma_score_distributions(feedbacks)
+        before_feedback = get_kdma_score_distributions(
+                            find_feedback(case["actions"][:-1], training_data, scenario, target))
+        after_feedback.pop("score")
+        new_case = new_case | flatten("feedback", after_feedback)
+        dfeedback = {key:subtract_dict(value, before_feedback[key]) for (key, value) in after_feedback.items() if type(value) == dict}
+        new_case = new_case | flatten("feedback_delta", dfeedback)
+        ret_cases.append(new_case)
+    write_case_base(fname, ret_cases)
+    
+def write_alignment_target_cases_to_csv(fname: str, training_data: list[dict[str, Any]]):
+    score_cases = []
+    for datum in training_data:
+        case = dict()
+        case["scenario_id"] = datum["scenario_id"]
+        case["target"] = datum["feedback"]["target"]
+        case["score"] = datum["feedback"]["score"]
+        case = case | flatten("kdma", datum["feedback"]["kdmas"])
+        score_cases.append(case)
+    write_case_base(fname, score_cases)
+    
+def subtract_dict(dict1: dict[str, Any], dict2: dict[str, Any]) -> dict[str, Any]:
+    return {key1:value1-dict2[key1] for (key1, value1) in dict1.items()}
