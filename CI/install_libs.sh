2b07ea6f13e79366619b05129b79e9c1b4e37bf4
Ray Kulhanek <ray.kulhanek@parallaxresearch.org>
2024-02-21 10:45:41 -0500

    ci stuff

--- CI/install_libs.sh
+++ CI/install_libs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Installs non-python dependencies. Assumes apt exists.
 
-sudo apt install -y libgomp1 wget
+sudo apt-get install -y libgomp1 wget sbcl
 
 wget https://beta.quicklisp.org/quicklisp.lisp 
-apt-get install -y sbcl
 yes "" | sbcl --load quicklisp.lisp --eval "(progn (quicklisp-quickstart:install) (eval (read-from-string \"(quicklisp:add-to-init-file)\")) (quit))" 
