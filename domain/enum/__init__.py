1707eb29279eee65f21a1148f4c5f9f758dba9ae
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2024-02-26 17:36:56 -0500

    Updated client to latest to correct spo2 errors, started using enums.

--- domain/enum/__init__.py
+++ domain/enum/__init__.py
@@ -2,6 +2,7 @@ from swagger_client.models.action_type_enum import ActionTypeEnum
 from swagger_client.models.supply_type_enum import SupplyTypeEnum
 from swagger_client.models.injury_type_enum import InjuryTypeEnum
 from swagger_client.models.injury_location_enum import InjuryLocationEnum
+from swagger_client.models.injury_status_enum import InjuryStatusEnum
 from swagger_client.models.mental_status_enum import MentalStatusEnum
 from swagger_client.models.heart_rate_enum import HeartRateEnum
 from swagger_client.models.breathing_level_enum import BreathingLevelEnum
