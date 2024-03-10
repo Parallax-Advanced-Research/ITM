5159ef1ea0e8be0a26c51e0bf2a5cc531567be74
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2024-03-09 23:03:20 -0500

    Moved server checking code out of tad_tester, into tad.py for greater accessibility.

--- tad_tester.py
+++ tad_tester.py
@@ -5,33 +5,11 @@ from runner.eval_driver import EvaluationDriver
 from scripts.shared import parse_default_arguments
 
 
+
 def main():
     args = parse_default_arguments()
     if args.endpoint is None:
-        ta3_port = util.find_environment("TA3_PORT", 8080)
-        if not util.is_port_open(ta3_port):
-            util.logger.error("TA3 server not listening. Shutting down.")
-            sys.exit(1)
-        check_adept = False
-        check_soartech = False
-        if args.session_type == 'eval':
-            check_adept = True
-            check_soartech = True
-        if args.training and args.session_type == 'adept':
-            check_adept = True
-        if args.training and args.session_type == 'soartech':
-            check_adept = True
-            
-        if check_adept:
-            adept_port = util.find_environment("ADEPT_PORT", 8081)
-            if not util.is_port_open(adept_port):
-                util.logger.error("ADEPT server not listening. Shutting down.")
-                sys.exit(1)
-        if check_soartech:
-            adept_port = util.find_environment("SOARTECH_PORT", 8084)
-            if not util.is_port_open(adept_port):
-                util.logger.error("ADEPT server not listening. Shutting down.")
-                sys.exit(1)
+        tad.check_for_servers()
 
     tad.api_test(args, EvaluationDriver(args))
 
