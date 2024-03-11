b20520efcd6d83dec7f963fd432aca196caecd47
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2024-03-10 23:07:03 -0400

    Corrected call to check_for_servers.

--- tad_tester.py
+++ tad_tester.py
@@ -9,7 +9,7 @@ from scripts.shared import parse_default_arguments
 def main():
     args = parse_default_arguments()
     if args.endpoint is None:
-        tad.check_for_servers()
+        tad.check_for_servers(args)
 
     tad.api_test(args, EvaluationDriver(args))
 
