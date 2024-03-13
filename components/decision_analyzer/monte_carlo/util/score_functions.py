b86a8b8f706b10389f7bce8655092307fe2935a0
jt <jt.turner@knexusresearch.com>
2024-03-13 14:15:33 -0400

    Smol medical soundness v2 is enabled

--- components/decision_analyzer/monte_carlo/util/score_functions.py
+++ components/decision_analyzer/monte_carlo/util/score_functions.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 
+from components.decision_analyzer.monte_carlo.medsim.util.medsim_enums import Metric
 from components.decision_analyzer.monte_carlo.mc_sim import MCState
 from components.decision_analyzer.monte_carlo.mc_sim.mc_tree import ScoreT
 from components.decision_analyzer.monte_carlo.medsim.util.medsim_state import MedsimState
@@ -87,3 +88,11 @@ def prob_death_after_minute(state: MCState) -> ScoreT:
         update_morbidity_calculations(cas)
     return med_prob_death(notreal_state)
 
+def prob_death_standard_time(state: MCState) -> ScoreT:
+    this_state = deepcopy(state)
+    time_remaining = max(0, Metric.STANDARD_TIME.value - this_state.time)
+    for casualty in this_state.casualties:
+        for injury in casualty.injuries:
+            update_smol_injury(injury, time_remaining, injury.treated)
+        update_morbidity_calculations(casualty)
+    return tiny_med_severity_score(this_state) + this_state.time
