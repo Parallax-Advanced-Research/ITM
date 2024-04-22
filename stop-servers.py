8058b76b814d3da9a2c6c2f4d95717a302ed4a29
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2024-04-22 16:28:46 -0400

    Fixed error.

--- stop-servers.py
+++ stop-servers.py
@@ -4,6 +4,7 @@ import glob
 import signal
 import subprocess
 import argparse
+import util
 
 ta3_port = util.find_environment("TA3_PORT", 8080)
 adept_port = util.find_environment("ADEPT_PORT", 8081)
@@ -14,16 +15,20 @@ parser = argparse.ArgumentParser(description="Runs an experiment attempting to l
                                              "themselves first.")
 parser.add_argument("--all", action=argparse.BooleanOptionalAction, default=False,
                     help="Stop all servers, not just the ones on the known ports.")
+parser.add_argument("--port", type=str, default=None,
+                    help="Stop servers at a given port.")
 args = parser.parse_args()
 
 if args.all:
     pid_files = glob.glob(os.path.join(".deprepos", "*.pid"))
+elif args.port is not None:
+    pid_files = glob.glob(os.path.join(".deprepos", f"*-{args.port}.pid"))
 else:
     soartech_file = os.path.join(".deprepos", f"ta1-server-mvp-{soartech_port}.pid")
     if os.path.exists(soartech_file):
         p = subprocess.run(["docker", "compose", "-f", "docker-compose-dev.yaml", "down"],
                              cwd=os.path.join(".deprepos", "ta1-server-mvp"))
-    pid_file = [soartech_file]
+    pid_files = [soartech_file]
     pid_files.append(os.path.join(".deprepos", f"itm-evaluation-server-{ta3_port}.pid"))
     pid_files.append(os.path.join(".deprepos", f"adept_server-{adept_port}.pid"))
 
