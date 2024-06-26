bdcf96e8c1b1e6df6aa439d5c178a2fa88b54bec
cr625 <cr625@drexel.edu>
2024-06-26 12:20:23 -0400

    add example image and create tables for output

--- components/decision_explainer/kdma_decision/kdma_decision_explainer.py
+++ components/decision_explainer/kdma_decision/kdma_decision_explainer.py
@@ -7,6 +7,7 @@ class KDMADecisionExplainer(DecisionExplainer):
         self.variant = "KDMA"
         self.active_action = None
         self.decision_attributes = {}
+        self.nn_attributes = {}        
 
     def explain_variant(self, decision: Decision, probe: TADProbe):
         # the explanation values dict put in the selected decision (minDecision) by the decision selector
@@ -15,20 +16,24 @@ class KDMADecisionExplainer(DecisionExplainer):
         # in the decision value is the :Action that was selected by the decision selector
         self.active_action : Action = decision.value # the chosen action from the the case constructed from the probe response
 
-        ''' 
-        # TODO: we can do this when we display the values from the file
-        similarity = explanation_values["DISTANCE"]
+        # get the most similar nearest neighbor for comparison
+        neighbors = explanation_values["NEAREST_NEIGHBORS"]
+        
+        # sort the neighbors by similarity
+        neighbors.sort(key=lambda x: x[0], reverse=True)
 
-        # kdmas predicted based on the active action 
-        predicted_kdmas = explanation_values["BEST_KDMAS"]
+        # get the most similar neighbor which is the first one in the sorted list
+        most_similar_neighbor = neighbors[0]
 
-        # Extract the nearest neighbor with the highest similarity from the explanation values for the example
-        # Just return the first one since the distances are all 0 for now
-        top_neighbor_similarity = explanation_values["NEAREST_NEIGHBORS"][0][0]
-        top_neighbor = explanation_values["NEAREST_NEIGHBORS"][0][1] 
-        '''        
+        # get the similarity value
+        nn_similarity = most_similar_neighbor[0] # is 'DISTANCE' the average?
+
+        # get the attributes of the nearest neighbor
+        self.nn_attributes.update(most_similar_neighbor[1]) # these are values rather than decision metrics object but get_values() can be used to get the values also
+        
+
         self.decision_attributes.update(decision.metrics)
-        # self.decision_attributes.update({"similarity": similarity, "predicted_kdmas": predicted_kdmas, "top_neighbor_similarity": top_neighbor_similarity, "top_neighbor": top_neighbor})
+        # we could also get the values from decision metrics objects here
         
         # map the metrics onto the weights to get the related values
         # we can get them from the decision.metrics except for the kdma specific ones
@@ -36,18 +41,53 @@ class KDMADecisionExplainer(DecisionExplainer):
         # the top x weights and their related values
         top_weights = self.process_weights(explanation_values["WEIGHTS"])
         top_properties = self.get_related_values(top_weights, probe)
-        explanation_values.update({"TOP_PROPERTIES":top_properties})
+        
+        nn_top_properties = self.get_related_nn_values(top_weights, self.nn_attributes)
+
+       
+        # the name of the kdma is the value of the dictionary key        
+        active_kdma_name = next(iter(explanation_values["BEST_KDMAS"])) # TODO what if there are multiple
+       
+        # the kdma value of the nearest neighbor
+        nn_kdma = self.nn_attributes[active_kdma_name]
+
+        # the name of the action of the nearest neighbor
+        nn_action = most_similar_neighbor[1]["action"] # the action name of the nearest neighbor
+        
+
+        # add the values to the explanation values so they can be serialized
+        # Maybe we can just send a subset of the values that we need in a new dictionary
         explanation_values.update({"TOP_WEIGHTS":top_weights})        
-        explanation = Explanation(self.variant,explanation_values) # the variant could go in the baseline definition
-        # you can also pass all of the nearest neighbors and their similarities to the explanation here if you need them
+        explanation_values.update({"TOP_PROPERTIES":top_properties})
+        explanation_values.update({"NN_TOP_PROPERTIES":nn_top_properties})
+        explanation_values.update({"NN_KDMA":nn_kdma})
+        explanation_values.update({"NN_ACTION":nn_action})
+        explanation_values.update({"NN_SIMILARITY":nn_similarity})
+           
+
+        explanation = Explanation(self.variant,explanation_values)
         return explanation
     
+
+
+
     # process the weights dictionary and only return the top 3 weights
     def process_weights(self, weights, top=3):
         sorted_weights = sorted(weights.items(), key=lambda x: x[1], reverse=True)
         top_weights = sorted_weights[:top]
         return top_weights
     
+
+    # get the related values of the top weights from the nearest neighbor attributes
+    def get_related_nn_values(self, top_weights, nn_attributes):
+        related_values = {}
+        attributes = nn_attributes
+        for weight in top_weights:
+            weight_name = weight[0]            
+            value = self.get_value_from_object(attributes, weight_name)
+            related_values[weight_name] = value
+        return related_values
+
     # get the related values of the top weights from the decision.metrics
     def get_related_values(self, top_weights, probe):
         related_values = {}
--- show_explanation.py
+++ show_explanation.py
@@ -3,6 +3,8 @@
 
 import pickle
 from tabulate import tabulate
+import ast
+import random
 
 input_file = "components/probe_dumper/tmp/MetricsEval.MD1-Urban.pkl"
 # open the pickle file in read binary mode
@@ -13,30 +15,59 @@ with open(input_file, 'rb') as f:
     made_decisions = data.made_decisions
 
     #select which decision to show
-    example_decision = made_decisions[0] # select the first decision
+    example_decision = random.choice(made_decisions)
 
     # get the explanation values for the decision
     expl_vals = example_decision.explanation_values
 
-    # get the decision name
-    print()
-    print(example_decision.value.name)
-    print("Similarity to Nearest Neighbor {}".format(expl_vals[0]["DISTANCE"])) 
-    # name and value of kdmas predicted based on the active action
-    best_kdmas = expl_vals[0]["BEST_KDMAS"]
-    print("KDMAS")
-    print(tabulate(best_kdmas.items(), headers=["KDMA", "Value"]))
-    print("\nTop Relevant Features")
+    # get the active scenario decision name of the selected example
+    active_scenario_action_name = example_decision.value.name
+    neighbor_action_name = ast.literal_eval(expl_vals[0]["NN_ACTION"]).get("name") # we should be consistent in the way we store the properties
+    neighbor_similarity = expl_vals[0]["NN_SIMILARITY"]
+
+    similarity_to_average = expl_vals[0]["DISTANCE"] # is 'DISTANCE' the average?
+    active_kdmas = expl_vals[0]["BEST_KDMAS"] # just the first one for the example
     
-    # print top weights and top properties as a table
+    # get the name of the kdma from active_kdmas
+    active_kdma_name = next(iter(active_kdmas)) # TODO what if there are multiple
+    # get the value of the kdma
+    active_kdma_value = active_kdmas[active_kdma_name]
+    
+    # the first row of a table with the columns "Decision","Active Scenario","Actual Example"
+    headers = ["","Active Scenario","Actual Example"]
+    # the second row of the table with the values of the decision, active scenario, and actual example
+    values = ["Decision", active_scenario_action_name,neighbor_action_name]
+
+    # the third row of the table with the values of the "", similarity_to_average, 
+    values2 = ["Similarity", similarity_to_average,neighbor_similarity]
+    
+    values3 = [active_kdma_name,active_kdma_value,expl_vals[0]["NN_KDMA"]]
+
+    # create a table in tabulate from the headers and values
+    print(tabulate([values,values2, values3], headers=headers, tablefmt="fancy_grid"))
+    
+    # get the top weights from the explanation values
     top_weights = expl_vals[0]["TOP_WEIGHTS"]
+    # get the top properties from the explanation values
     top_properties = expl_vals[0]["TOP_PROPERTIES"]
-    
 
-    # zip the two lists together
-    zipped = list(zip(top_weights, top_properties.values()))
-    # print the table
-    print(tabulate(zipped, headers=["Top Weights", "Top Properties"],tablefmt="fancy_grid")) # the top properties look redundant here but if they were numerical they would correspond to the weights
+    # get the top properties from the nearest neighbor
+    nn_top_properties = expl_vals[0]["NN_TOP_PROPERTIES"]
 
+    # the headers for the table
+    headers = ["Features","Relevance","Active Scenario","Actual Example"]
 
-    print()
+    # create a row for each feature in the top weights
+    rows = []
+    for i in range(len(top_weights)):
+        # get the feature and its weight
+        feature, weight = top_weights[i]
+        # get the value of the feature from the top properties
+        value = top_properties[feature]
+        # get the value of the feature from the nearest neighbor top properties
+        nn_value = nn_top_properties[feature]
+        # create a row with the feature, weight, value, and nearest neighbor value
+        rows.append([feature,weight,value,nn_value])
+
+    # create a table in tabulate from the headers and rows
+    print(tabulate(rows, headers=headers, tablefmt="fancy_grid"))
