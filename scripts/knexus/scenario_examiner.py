ec1e8b31e0840443b74ba3385e33e51920fd7066
John Meyer <john.meyer@knexusresearch.com>
2024-07-11 09:53:49 -0600

    gather info on new supplies

--- scripts/knexus/scenario_examiner.py
+++ scripts/knexus/scenario_examiner.py
@@ -14,9 +14,19 @@ def get_injuries(probe):
     injury_list = sorted(list(injury_set))
     return injury_list
 
+
+def get_supplies(probe):
+    supply_set = set()
+    sup_list = probe['state']['supplies']
+    for sup in sup_list:
+        supply_set.add(sup['type'])
+    return supply_set
+
+
 if __name__ == '__main__':
     print(os.listdir(osp.join('data', 'july_scens')))
     all_inj = []
+    all_supplies = []
     for scenario_file in os.listdir(osp.join('data', 'july_scens')):
         print(scenario_file)
         fname = open(osp.join('data', 'july_scens', scenario_file))
@@ -26,7 +36,14 @@ if __name__ == '__main__':
         # for inj in inj_list:
         #     print(inj)
         # print('---\n\n\n')
+        supply_list = get_supplies(as_json)
+        all_supplies.extend(supply_list)
     all_inj = sorted(list(set(all_inj)))
     for inj in all_inj:
         print(inj)
+    print('---\n\n\n')
+
+    all_supplies = sorted(list(set(all_supplies)))
+    for sup in all_supplies:
+        print(sup)
     print('---\n\n\n')
