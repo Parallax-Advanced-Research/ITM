6ea495870f41e61cfb942da2d5d959ebacad99e8
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2024-08-20 12:10:19 -0400

    Changed call to analyze_data to handle new api.

--- ta3_training.py
+++ ta3_training.py
@@ -59,10 +59,13 @@ def main():
         driver.actions_performed = []
         driver.treatments = {}
         args.runs -= 1
-    analyze_data.do_analysis_search(
+    kdma_cases = analyze_data.analyze_pre_cases(
         "temp/pretraining_cases.json", None, "temp/kdma_cases.csv", 
-        "temp/alignment_target_cases.csv", "temp/kdma_weights.json", "temp/all_weights.json", 
-        "probability" if args.session_type == "soartech" else "avgdiff")
+        "temp/alignment_target_cases.csv")
+    analyze_data.do_weight_search(
+        kdma_cases, "temp/kdma_weights.json", "temp/all_weights.json", 
+        "probability" if args.session_type == "soartech" else "avgdiff", 
+        "temp/pretraining_cases.csv")
     
 def output_training_cases():
     (cases, training_data) = analyze_data.read_training_data()
