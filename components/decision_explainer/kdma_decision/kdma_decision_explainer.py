60322146363e8d9d3a9a74a90176c63f9dc9c9f0
cr625 <cr625@drexel.edu>
2024-08-20 18:57:35 -0400

    cleanup and refactor

--- components/decision_explainer/kdma_decision/kdma_decision_explainer.py
+++ components/decision_explainer/kdma_decision/kdma_decision_explainer.py
@@ -1,8 +1,8 @@
 import ast
 from domain.internal import Decision, Explanation
 from components import DecisionExplainer
-from typing import Dict, Any
 from .explanation_text import generate_explanation_text
+
 class KDMADecisionExplainer(DecisionExplainer):
     def __init__(self):
         super().__init__()
@@ -11,72 +11,55 @@ class KDMADecisionExplainer(DecisionExplainer):
         self.explanation = None
 
     def explain(self, decision: Decision):
-        # the list of explanation items is added by the decision selector at runtime and stores the values used in the decide method
-        self.decision = decision        
-        for explanation_item in self.decision.explanations:
-            # if it is not a list
-            if isinstance(explanation_item, Explanation):            
-                if explanation_item.explanation_type == "kdma_estimation":                
-                    self.explanation = explanation_item
-                    return self.return_description(self.explanation)            
-        return None
-       
-    def return_description(self, explanation):
-        output = "Could not generate explanation"
-        # the attributes used in the retrieval of the most similar case
-        weight_settings = explanation.params["weight_settings"]["kdma_specific_weights"]
-        
-        # extract the inner dictionary from weight_settings        
-        weights = {k: v for inner_dict in weight_settings.values() for k, v in inner_dict.items()}
-        
-        # top 5 by value in weights
-        relevant_attributes = [k for k, v in sorted(weights.items(), key=lambda item: item[1], reverse=True)][:5]
-               
-        '''
-        selected decision
-        '''
-        # these are the attributes for is the selected decision combined so that we can compare to the similar case
-        decision_action = self.decision.value # this is the action object that is associated with the decision        
-        decision_attributes = {} # a dictionary to hold the decision attributes of the chosen decision to comare to the similar case
-        # start collecting the attributes of the decision
-        decision_attributes.update(explanation.params["best_case"])        
-        # create a sparse new_instance dictionary representing the selected decision for Anik's code
-        new_instance = {k: v for k, v in decision_attributes.items() if k in relevant_attributes} # only select the attributes that have weights that were used in the similarity calculation
-        new_instance.update(decision_action.params)
-        action_new_instance = decision_action.name
-        
-        '''
-        most similar case
-        '''       
-        # most similar cases saved from decision selector     
-        similar_cases = explanation.params.get("similar_cases")
-        # sort the similar cases by distance
-        # if there are no similar cases skip this loop
-        if similar_cases:
-                
-            similar_cases.sort(key=lambda x: x[0])
-            
-            # similar_case_attributtes is the dictionary in the tuple
-            similar_case_attributes = similar_cases[0][1] # the first tuple in the sorted list is the most similar case
-            
-            # see if similar_case_attributes has an action key
-            if "action" in similar_case_attributes:                
-                similar_case_action = similar_case_attributes.pop("action") # remove the action from the similar case attributes and append it to the similar_case_action_params so it matches the new_instance
+        self.decision = decision
+        self.explanation = self.get_explanation()
+        return self.return_description(self.explanation) if self.explanation else None
 
-                #interpret similar_case_action_params as a dictionary
-                similar_case_action_params = ast.literal_eval(similar_case_action) # this was stored as a string so convert to a dictionary
-                similar_case_action_name = similar_case_action_params["name"]
-                #similar_case_attributes.update(similar_case_action_params["params"])
-                
-                # extract the attributes that are relevant to the similarity calculation    
-                most_similar_instance = {k: v for k, v in similar_case_attributes.items() if k in relevant_attributes}
-                most_similar_instance.update(similar_case_action_params["params"])
-                action_most_similar = similar_case_action_name
-                
-                output = generate_explanation_text(new_instance, most_similar_instance, action_new_instance, action_most_similar)
+    def get_explanation(self):
+        return next(
+            (item for item in self.decision.explanations if isinstance(item, Explanation) and item.explanation_type == "kdma_estimation"),
+            None
+        )
+
+    def return_description(self, explanation: Explanation):
+        relevant_attributes = self.get_relevant_attributes(explanation)
+        new_instance, action_new_instance = self.get_new_instance(explanation, relevant_attributes)
+        most_similar_instance, action_most_similar = self.get_most_similar_instance(explanation, relevant_attributes)
+
+        if most_similar_instance:
+            output = generate_explanation_text(new_instance, most_similar_instance, action_new_instance, action_most_similar)
         else:
-            output = "No similar cases found"
+            output = "Could not generate explanation"
+
         print(output)
         return output
 
+    def get_relevant_attributes(self, explanation):
+        weight_settings = explanation.params["weight_settings"]["kdma_specific_weights"]
+        weights = {k: v for inner_dict in weight_settings.values() for k, v in inner_dict.items()}
+        return sorted(weights, key=weights.get, reverse=True)[:5]
 
+    def get_new_instance(self, explanation, relevant_attributes):
+        decision_action = self.decision.value
+        decision_attributes = explanation.params["best_case"]
+        new_instance = {k: v for k, v in decision_attributes.items() if k in relevant_attributes}
+        new_instance.update(decision_action.params)
+        return new_instance, decision_action.name
+
+    def get_most_similar_instance(self, explanation, relevant_attributes):
+        similar_cases = explanation.params.get("similar_cases")
+        if not similar_cases:
+            print("No similar cases found")
+            return None, None
+
+        similar_cases.sort(key=lambda x: x[0])
+        similar_case_attributes = similar_cases[0][1]
+
+        if "action" in similar_case_attributes:
+            similar_case_action_params = ast.literal_eval(similar_case_attributes.pop("action"))
+            similar_case_action_name = similar_case_action_params["name"]
+            most_similar_instance = {k: v for k, v in similar_case_attributes.items() if k in relevant_attributes}
+            most_similar_instance.update(similar_case_action_params["params"])
+            return most_similar_instance, similar_case_action_name
+        else:
+            return None, None
--- components/decision_explainer/kdma_decision/select_template.py
+++ components/decision_explainer/kdma_decision/select_template.py
@@ -1,39 +1,50 @@
+def format_action(action):
+    return action.replace('_', ' ')
+
 def select(action_new_instance, action_most_similar_instance, treatment_new_instance=None, treatment_most_similar_instance=None, params=None):
     action = action_new_instance.lower()
     action_most_similar_instance = action_most_similar_instance.lower()
     template = ''
+
     if action == "apply_treatment":
-        action_str = action.replace('_', ' ')
-        action_most_similar_instance_str = action_most_similar_instance.replace('_', ' ')
-
-        treatment_new_instance = treatment_new_instance.lower()
-        treatment_new_instance = treatment_new_instance.replace('_', ' ')
-
-        if treatment_most_similar_instance is not None:
-            treatment_most_similar_instance = treatment_most_similar_instance.lower()
-            treatment_most_similar_instance = treatment_most_similar_instance.replace('_', ' ')
-
-            template = "I selected " + action_str + " with " + treatment_new_instance + " because I was reminded of a similar previous case where a similar decision maker decided to " + action_most_similar_instance_str + " with " + \
-                             treatment_most_similar_instance
-        else:
-            template = "I selected " + action_str + " with " + treatment_new_instance + " because I was reminded of a similar previous case where a similar decision maker decided to " + action_most_similar_instance_str + ""
+        template = apply_treatment_template(action, action_most_similar_instance, treatment_new_instance, treatment_most_similar_instance)
     elif action == "sitrep":
-        if action_most_similar_instance == 'sitrep':
-            action_most_similar_instance = 'situation report'
-        template = "I decided to submit a situation report to provide an update on the current status because I was reminded of a previous case where a similar decision maker decided to submit a " + action_most_similar_instance
+        template = sitrep_template(action_most_similar_instance)
     elif action == "tag_character":
-        casualty_name = params['casualty'].lower() if 'casualty' in params and params['casualty'] else 'unknown'
-        category = params['category'].lower() if 'category' in params and params['category'] else 'Unknown'
-        category_most_similar = params['category_most_similar'].lower() if 'category_most_similar' in params and params['category_most_similar'] else 'Unknown'
-        template = "I selected to tag "+casualty_name+" as "+category+" because I was reminded of a previous case where a similar decision maker decided to tag casualty as "+category_most_similar
-    elif action == "check_all_vitals" or action == "check_blood_oxygen" or action == "check_pulse" or action == "check_respiration":
-        action_str = action.replace('_', ' ')
-        action_most_similar_instance_str = action_most_similar_instance.replace('_', ' ')
-        template = "I selected to " + action_str + " because I was reminded of a previous case where a similar decision maker decided to " + action_most_similar_instance_str
+        template = tag_character_template(params)
+    elif action in {"check_all_vitals", "check_blood_oxygen", "check_pulse", "check_respiration"}:
+        template = check_vitals_template(action, action_most_similar_instance)
     elif action == 'move_to_evac':
         template = "I decided to move the casualty to the designated evacuation point because I was reminded of a previous case where a similar decision maker decided to transfer a casualty to an evacuation point"
     elif action == 'direct_mobile_characters':
         template = "I decided to direct mobile characters because I was reminded of a previous case where a similar decision maker directed responsive and alert characters for a mobility check"
     else:
-        print("Unknown action")
+        template = "Unknown action"
+
     return template
+
+def apply_treatment_template(action, action_most_similar_instance, treatment_new_instance, treatment_most_similar_instance):
+    action_str = format_action(action)
+    action_most_similar_instance_str = format_action(action_most_similar_instance)
+    treatment_new_instance = format_action(treatment_new_instance.lower())
+
+    if treatment_most_similar_instance:
+        treatment_most_similar_instance = format_action(treatment_most_similar_instance.lower())
+        return f"I selected {action_str} with {treatment_new_instance} because I was reminded of a similar previous case where a similar decision maker decided to {action_most_similar_instance_str} with {treatment_most_similar_instance}"
+    else:
+        return f"I selected {action_str} with {treatment_new_instance} because I was reminded of a similar previous case where a similar decision maker decided to {action_most_similar_instance_str}"
+
+def sitrep_template(action_most_similar_instance):
+    action_most_similar_instance = 'situation report' if action_most_similar_instance == 'sitrep' else action_most_similar_instance
+    return f"I decided to submit a situation report to provide an update on the current status because I was reminded of a previous case where a similar decision maker decided to submit a {action_most_similar_instance}"
+
+def tag_character_template(params):
+    casualty_name = params.get('casualty', 'unknown').lower()
+    category = params.get('category', 'unknown').lower()
+    category_most_similar = params.get('category_most_similar', 'unknown').lower()
+    return f"I selected to tag {casualty_name} as {category} because I was reminded of a previous case where a similar decision maker decided to tag casualty as {category_most_similar}"
+
+def check_vitals_template(action, action_most_similar_instance):
+    action_str = format_action(action)
+    action_most_similar_instance_str = format_action(action_most_similar_instance)
+    return f"I selected to {action_str} because I was reminded of a previous case where a similar decision maker decided to {action_most_similar_instance_str}"
