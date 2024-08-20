f79298b16988679e99cf9b553c56f77d66a89b9c
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2024-08-20 10:23:05 -0400

    Added Bounded Rationalizer features to ignore list.

--- components/decision_selector/kdma_estimation/triage_constants.py
+++ components/decision_selector/kdma_estimation/triage_constants.py
@@ -11,5 +11,6 @@ BASIC_WEIGHTS = {feature:1 for feature in BASIC_TRIAGE_CASE_FEATURES}
     
 IGNORE_PATTERNS = [
     'index', 'hash', 'feedback', 'action-len', 'justification', 'unnamed', 'nondeterminism', 
-    'hint', 'maximization', 'moraldesert', '.stdev', 'casualty_', 'scene'
+    'hint', 'maximization', 'moraldesert', '.stdev', 'casualty_', 'scene', 'hra strategy', 
+    'priority'
 ]
