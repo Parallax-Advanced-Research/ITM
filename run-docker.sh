0915c2b56cc5702e9865c5f464a33bdd6c38d151
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2024-08-27 23:03:10 -0400

    Updated run-docker.sh with new arguments.

--- run-docker.sh
+++ run-docker.sh
@@ -3,4 +3,4 @@ if [ -z "$TA3_ENDPOINT" ]
    then TA3_ENDPOINT="host.docker.internal:8080"
 fi
 
-docker run --env PYTHONPATH=. -it tad python3 scripts/tad_tester.py --endpoint $TA3_ENDPOINT $*
+docker run --env PYTHONPATH=. -it tad python3 tad_tester.py --session_type eval --no-training --selector keds --assessor triage --endpoint $TA3_ENDPOINT $*
