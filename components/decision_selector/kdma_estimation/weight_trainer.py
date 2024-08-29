8b77cccdc1e1a2fceca6c4f486aac2e5466f720b
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2024-08-29 12:00:53 -0400

    Shifted to use distanced error only.

--- components/decision_selector/kdma_estimation/weight_trainer.py
+++ components/decision_selector/kdma_estimation/weight_trainer.py
@@ -53,10 +53,10 @@ class KEDSModeller(CaseModeller):
             distanced_error = kdma_estimation.find_leave_one_out_error(
                                                 self.last_weights, self.kdma_name,
                                                 cases=self.cases, avg=self.use_average_error, reject_same_scene=True)
-            local_error = kdma_estimation.find_leave_one_out_error(
-                                                self.last_weights, self.kdma_name,
-                                                cases=self.cases, avg=self.use_average_error, reject_same_scene=False)
-            self.last_error = (distanced_error + local_error) / 2
+            # local_error = kdma_estimation.find_leave_one_out_error(
+                                                # self.last_weights, self.kdma_name,
+                                                # cases=self.cases, avg=self.use_average_error, reject_same_scene=False)
+            self.last_error = distanced_error
         return self.last_error
     
     def get_state(self) -> dict[str, Any]:
