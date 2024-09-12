6dc97360dbaf3f9551657896fb60c2068bbc97b1
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2024-09-12 15:29:42 -0400

    Added argument support to severity_decision_selector.

--- components/decision_selector/severity/severity_decision_selector.py
+++ components/decision_selector/severity/severity_decision_selector.py
@@ -7,8 +7,17 @@ from triage import TriageCompetenceAssessor
 import util
 
 class SeverityDecisionSelector(DecisionSelector):
-    def __init__(self):
+    def __init__(self, args = None):
         self.competence_assessor = TriageCompetenceAssessor()
+        self.insert_pauses = False
+        self.decision_verbose = False
+        if args is not None:
+            self.initialize_with_args(args)
+
+    def initialize_with_args(self, args):
+        self.insert_pauses = args.insert_pauses
+        self.decision_verbose = args.decision_verbose
+
 
     def select(self, scenario: Scenario, probe: TADProbe, target: AlignmentTarget) -> (Decision, float):
         maxSeverityChange = -math.inf
@@ -20,6 +29,9 @@ class SeverityDecisionSelector(DecisionSelector):
             best_competence = max(competences.values())
 
         for decision in probe.decisions:
+            if self.decision_verbose:
+                print(f"Decision: {decision.value}")
+                print(f"Competence: {competences[str(decision.value)]}")
             if self.competence_assessor is not None and best_competence > competences[str(decision.value)]:
                 # Just skip less competent actions.
                 continue
@@ -29,6 +41,9 @@ class SeverityDecisionSelector(DecisionSelector):
                 if curChange > maxSeverityChange:
                     maxSeverityChange = curChange
                     bestDecision = decision
+                if self.decision_verbose:
+                    print(f"Severity change: {curChange}")
+                
                 
         if bestDecision is None: 
             poss_decisions = probe.decisions
@@ -41,6 +56,8 @@ class SeverityDecisionSelector(DecisionSelector):
         if self.competence_assessor is not None:
             print(f"Competence: {competences[str(bestDecision.value)]}")
         print(f"Severity change: {maxSeverityChange}")
+        if self.insert_pauses:
+            breakpoint()
         return (bestDecision, maxSeverityChange)
         
     def add_assessor(self, name: str, assessor: Assessor):
