5c72cb0399d969c31c85e69c56d3b36fc319d371
cr625 <cr625@drexel.edu>
2024-11-15 09:15:45 -0500

    clean up code and prepare to merge feature/ph1

--- triage/triage_competence_assessor.py
+++ triage/triage_competence_assessor.py
@@ -7,17 +7,7 @@ from domain.enum import ActionTypeEnum, SupplyTypeEnum, TagEnum, InjuryTypeEnum,
     MentalStatusEnum, HeartRateEnum, BloodOxygenEnum, AvpuLevelEnum, \
     BreathingLevelEnum, ParamEnum
 
-from .domain_reference import TriageCategory, TreatmentsEnum, InjuryLocationEnum, InjurySeverityEnum
-
-
-CHECK_ACTION_TYPES = [ActionTypeEnum.CHECK_ALL_VITALS, ActionTypeEnum.CHECK_BLOOD_OXYGEN,
-                      ActionTypeEnum.CHECK_PULSE, ActionTypeEnum.CHECK_RESPIRATION,
-                      ActionTypeEnum.SITREP, ActionTypeEnum.SEARCH, ActionTypeEnum.MOVE_TO]
-
-PAINMED_SUPPLIES = {  # Define available pain meds supplies that may be administered
-    TreatmentsEnum.PAIN_MEDICATIONS,
-    TreatmentsEnum.FENTANYL_LOLLIPOP
-}
+from .domain_reference import TriageCategory, TreatmentsEnum, InjuryLocationEnum, InjurySeverityEnum, CHECK_ACTION_TYPES, PAINMED_SUPPLIES
 
 
 class TriageCompetenceAssessor(Assessor):
@@ -96,6 +86,7 @@ class TriageCompetenceAssessor(Assessor):
                     -self.get_max_injury_severity(a['casualty'])
                 )
             )
+            # TODO: What if the severities are the same. Also, maybe don't rank tag actions?
 
             # Step 3: Adjust all but the highest-ranked assessment's score
             for assessment in high_competence_assessments[1:]:
@@ -932,24 +923,6 @@ Triage Competence Assessor Original functions
 '''
 
 
-def get_neediest_tag(probe: TADProbe):
-    neediest_tag = TagEnum.MINIMAL
-    for ch in probe.state.casualties:
-        ch_tag = max(get_tags(ch), key=neediness)
-        if patient_treatable(probe, ch) and neediness(ch_tag) >= neediness(neediest_tag):
-            neediest_tag = ch_tag
-    return neediest_tag
-
-
-def get_neediest_transfer_tag(probe: TADProbe):
-    neediest_tag = TagEnum.MINIMAL
-    for ch in probe.state.casualties:
-        ch_tag = max(get_tags(ch), key=neediness)
-        if neediness(ch_tag) >= neediness(neediest_tag):
-            neediest_tag = ch_tag
-    return neediest_tag
-
-
 def patient_treatable(probe: TADProbe, ch: Casualty):
     return id_treatable(probe, ch.id)
 
@@ -959,22 +932,6 @@ def id_treatable(probe: TADProbe, id: str):
         if is_treatment_action(dec.value) and id == dec.value.params[ParamEnum.CASUALTY]:
             return True
     return False
-
-
-TAG_NEED_MAP = {TagEnum.IMMEDIATE: 10, TagEnum.DELAYED: 5,
-                TagEnum.EXPECTANT: 2, TagEnum.MINIMAL: 1}
-
-
-def neediness(tag: str):
-    return TAG_NEED_MAP[tag]
-
-
-TAG_SERIOUS_MAP = {TagEnum.EXPECTANT: 10,
-                   TagEnum.IMMEDIATE: 7, TagEnum.DELAYED: 5, TagEnum.MINIMAL: 1}
-
-
-def seriousness(tag: str):
-    return TAG_SERIOUS_MAP[tag]
 
 
 def is_treatment_action(act: Action):
@@ -1003,135 +960,3 @@ def get_target_patient(probe: TADProbe, dec: Decision):
         if ch.id == cas_id:
             return ch
     return None
-
-
-def get_tags(patient: Casualty) -> str:
-    if patient.tag is not None:
-        return [patient.tag]
-    else:
-        return determine_tag(patient)
-
-
-def determine_tag(patient: Casualty) -> list[str]:
-    tags = [TagEnum.MINIMAL]
-    for inj in patient.injuries:
-        tags = get_worst_tags(get_injury_tags(inj), tags)
-    tags = get_worst_tags(get_tags_breathing(patient.vitals.breathing), tags)
-    tags = get_worst_tags(get_tags_mental_status(
-        patient.vitals.mental_status), tags)
-    tags = get_worst_tags(get_tags_spo2(patient.vitals.spo2), tags)
-    tags = get_worst_tags(get_tags_heart_rate(patient.vitals.hrpmin), tags)
-    tags = get_worst_tags(get_tags_avpu(patient.vitals.avpu), tags)
-    return tags
-
-
-BREATHING_TAGS = {
-    BreathingLevelEnum.NORMAL: [TagEnum.MINIMAL],
-    BreathingLevelEnum.FAST: [TagEnum.DELAYED],
-    BreathingLevelEnum.SLOW: [TagEnum.IMMEDIATE],
-    BreathingLevelEnum.RESTRICTED: [TagEnum.IMMEDIATE],
-    BreathingLevelEnum.NONE: [TagEnum.IMMEDIATE]
-}
-
-
-def get_tags_breathing(breathing: str) -> list[str]:
-    return BREATHING_TAGS.get(breathing, [TagEnum.MINIMAL])
-
-
-MENTAL_STATUS_TAGS = {
-    MentalStatusEnum.SHOCK: [TagEnum.IMMEDIATE],
-    MentalStatusEnum.UNRESPONSIVE: [TagEnum.IMMEDIATE],
-    MentalStatusEnum.AGONY: [TagEnum.DELAYED]
-}
-
-
-def get_tags_mental_status(mental_status: str) -> list[str]:
-    return MENTAL_STATUS_TAGS.get(mental_status, [TagEnum.MINIMAL])
-
-
-OXYGEN_TAGS = {
-    BloodOxygenEnum.LOW: [TagEnum.IMMEDIATE],
-    BloodOxygenEnum.NONE: [TagEnum.IMMEDIATE]
-}
-
-
-def get_tags_spo2(spo2: str) -> list[str]:
-    return OXYGEN_TAGS.get(spo2, [TagEnum.MINIMAL])
-
-
-HEART_RATE_TAGS = {
-    HeartRateEnum.FAST: [TagEnum.DELAYED],
-    HeartRateEnum.FAINT: [TagEnum.IMMEDIATE],
-    HeartRateEnum.NONE: [TagEnum.IMMEDIATE]
-}
-
-
-def get_tags_heart_rate(heart_rate: str) -> list[str]:
-    return HEART_RATE_TAGS.get(heart_rate, [TagEnum.MINIMAL])
-
-
-AVPU_TAGS = {
-    AvpuLevelEnum.ALERT: [TagEnum.MINIMAL],
-    AvpuLevelEnum.VOICE: [TagEnum.DELAYED],
-    AvpuLevelEnum.PAIN: [TagEnum.DELAYED],
-    AvpuLevelEnum.UNRESPONSIVE: [TagEnum.IMMEDIATE]
-}
-
-
-def get_tags_avpu(avpu: str) -> list[str]:
-    return AVPU_TAGS.get(avpu, [TagEnum.MINIMAL])
-
-
-def get_injury_tags(injury: Injury) -> list[str]:
-    if injury.name == InjuryTypeEnum.EAR_BLEED:
-        return [TagEnum.EXPECTANT]
-    elif injury.name == InjuryTypeEnum.ASTHMATIC:
-        return [TagEnum.MINIMAL]
-    elif injury.name == InjuryTypeEnum.CHEST_COLLAPSE:
-        return [TagEnum.IMMEDIATE]
-    elif injury.name == InjuryTypeEnum.AMPUTATION:
-        return [TagEnum.IMMEDIATE]
-    elif injury.name == InjuryTypeEnum.OPEN_ABDOMINAL_WOUND:
-        return [TagEnum.IMMEDIATE]
-    elif injury.name == InjuryTypeEnum.TRAUMATIC_BRAIN_INJURY:
-        return [TagEnum.DELAYED]
-    elif injury.name == InjuryTypeEnum.BROKEN_BONE:
-        return [TagEnum.DELAYED]
-    elif injury.name == InjuryTypeEnum.INTERNAL:
-        return [TagEnum.DELAYED, TagEnum.IMMEDIATE]
-    elif injury.name == InjuryTypeEnum.BURN:
-        if injury.severity in [InjurySeverityEnum.MINOR]:
-            return [TagEnum.MINIMAL]
-        elif injury.severity in [InjurySeverityEnum.MODERATE]:
-            return [TagEnum.DELAYED]
-        elif injury.severity in [InjurySeverityEnum.SUBSTANTIAL, InjurySeverityEnum.MAJOR]:
-            return [TagEnum.IMMEDIATE]
-        elif injury.severity in [InjurySeverityEnum.EXTREME]:
-            return [TagEnum.EXPECTANT]
-    elif injury.name in [InjuryTypeEnum.LACERATION, InjuryTypeEnum.ABRASION]:
-        if injury.severity in [InjurySeverityEnum.MINOR]:
-            return [TagEnum.MINIMAL]
-        elif injury.severity in [InjurySeverityEnum.MODERATE]:
-            return [TagEnum.DELAYED]
-        elif injury.severity in [InjurySeverityEnum.SUBSTANTIAL, InjurySeverityEnum.MAJOR]:
-            return [TagEnum.IMMEDIATE]
-        elif injury.severity in [InjurySeverityEnum.EXTREME]:
-            return [TagEnum.IMMEDIATE, TagEnum.EXPECTANT]
-    elif injury.name in [InjuryTypeEnum.PUNCTURE, InjuryTypeEnum.SHRAPNEL]:
-        if injury.severity in [InjurySeverityEnum.MINOR, InjurySeverityEnum.MODERATE]:
-            return [TagEnum.MINIMAL]
-        elif injury.severity in [InjurySeverityEnum.SUBSTANTIAL]:
-            return [TagEnum.DELAYED]
-        elif injury.severity in [InjurySeverityEnum.MAJOR]:
-            return [TagEnum.IMMEDIATE]
-        elif injury.severity in [InjurySeverityEnum.EXTREME]:
-            return [TagEnum.IMMEDIATE, TagEnum.EXPECTANT]
-
-
-def get_worst_tags(new_tags: list[str], old_tags: list[str]) -> bool:
-    most_serious_tag = max(new_tags + old_tags, key=seriousness)
-    if most_serious_tag not in old_tags:
-        return new_tags
-    if len(old_tags) > len(new_tags):
-        return new_tags
-    return old_tags
