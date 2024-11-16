066e46e6af95dc4eeea3c1ae2b9824f23469d0db
Matthew Molineaux <matthew,molineaux@parallaxresearch.org>
2024-11-16 18:26:08 -0500

    Fixed reading of empty error data.
    Corrected vote function.
    
    Removed committed breakpoint.

--- components/decision_selector/kdma_estimation/kdma_estimation_decision_selector.py
+++ components/decision_selector/kdma_estimation/kdma_estimation_decision_selector.py
@@ -126,8 +126,8 @@ class KDMAEstimationDecisionSelector(DecisionSelector):
     def initialize_weights(self, weight_filename):
         try:
             weight_settings_list = []
+            decoder = json.decoder.JSONDecoder()
             with open(weight_filename, "r") as weight_file:
-                decoder = json.decoder.JSONDecoder()
                 weight_json = weight_file.read()
             weight_json = weight_json.strip()
             while len(weight_json) > 0:
@@ -142,6 +142,8 @@ class KDMAEstimationDecisionSelector(DecisionSelector):
                     for record in json_obj["weights"]:
                         self.process_record(record, weight_settings_list)
                     self.error_data = json_obj["case_errors"]
+                    if self.error_data == "blank":
+                        self.error_data = None
                     case_hash = util.hashing.hash_file(self.case_file)
                     if case_hash != json_obj["case_base"]:
                         raise Exception("Weight file is not tuned for this case file.")
@@ -330,7 +332,7 @@ class KDMAEstimationDecisionSelector(DecisionSelector):
                 [(index_popularity[index], self.cb[int(index)-1])for index in top_indices[:triage_constants.DEFAULT_KDMA_NEIGHBOR_COUNT]], 
                 weight_trust_ratings)
 
-    def vote(self, kdma_name: str, individual_estimates: list[tuple[str, dict[float, float], list[dict[str, Any]]]], target: AlignmentTarget) -> tuple[dict[float, float], list[dict[str, Any]]]:
+    def vote(self, kdma_name: str, individual_estimates: list[tuple[str, dict[float, float], list[dict[str, Any]]]]) -> tuple[dict[float, float], list[dict[str, Any]]]:
         index_popularity = {}
         kdma_val_totals = {}
         total_prob = 0
@@ -338,8 +340,10 @@ class KDMAEstimationDecisionSelector(DecisionSelector):
         for (weight_id, kdma_val_probs, neighbors) in individual_estimates:
             if len(kdma_val_probs) == 0:
                 continue
-                
+            
+            total_dist = sum([dist for (dist, case) in neighbors])
             for (dist, neighbor) in neighbors:
+                nindex = neighbor["index"]
                 val = neighbor[kdma_name]
                 if total_dist == 0.0:
                     prob = 1 / len(neighbors)
@@ -350,7 +354,7 @@ class KDMAEstimationDecisionSelector(DecisionSelector):
                 total_prob += prob
                 
         if total_prob == 0.0:
-            return ({}, [], {})
+            return ({}, [])
 
         top_indices = sorted(index_popularity.keys(), key=index_popularity.get)
         return ({kdma_val: prob/total_prob for (kdma_val, prob) in kdma_val_totals.items()},
@@ -424,7 +428,7 @@ class KDMAEstimationDecisionSelector(DecisionSelector):
                         else:
                             kdma_val_probs, topK, ratings = self.trust_vote(kdma_name.lower(), individual_estimates, target)
                     else:
-                        kdma_val_probs, topK = self.vote(individual_estimates)
+                        kdma_val_probs, topK = self.vote(kdma_name.lower(), individual_estimates)
                     if len(kdma_val_probs) == 0:
                         kdma_val_probs, topK = self.make_minimum_error_estimate(kdma_name.lower(), individual_estimates)
                         
@@ -869,7 +873,6 @@ def make_case_triage(probe: TADProbe, d: Decision, variant: str) -> dict[str, An
             elif parts[1] == 'ph1':
                 case['scene'] = parts[0] + parts[3]
         case['scene'] += ":" + meta_block["scene_id"]
-    breakpoint()
     return case
 
 
