aa5fda23ca47307ec59640f9cc162b0c46316576
Chris Rauch <70023998+cr625@users.noreply.github.com>
2025-01-31 19:12:30 -0500

    add insurance models and analyzer

---
+++ components/decision_analyzer/insurance/insurance_decision_analyzer.py
@@ -0,0 +1,26 @@
+from domain.insurance.models.insurance_tad_probe import InsuranceTADProbe
+from domain.insurance.models.decision import Decision as InsuranceDecision
+from domain.insurance.models.decision_metric import DecisionMetric as InsuranceDecisionMetric
+from domain.insurance.models.scenario import Scenario
+from components.decision_analyzer.default.baseline_decision_analyzer import BaselineDecisionAnalyzer
+import random
+
+
+class InsuranceDecisionAnalyzer(BaselineDecisionAnalyzer):
+    def __init__(self):
+        super().__init__()
+
+    def analyze(self, scen: Scenario, probe: InsuranceTADProbe) -> dict[str, dict]:
+        analysis = {}
+        for decision in probe.decisions:
+            metrics = {
+                "Random": InsuranceDecisionMetric(
+                    name="Random",
+                    description="A random value",
+                    value=random.random()
+                )
+            }
+            # Update the metrics in the decision with our currently calculated metrics
+            decision.metrics.update(metrics)
+            analysis[decision.id_] = metrics
+        return analysis
